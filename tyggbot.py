a68f599e176eca5f8693d584a1b08e705311aa3d
Rasmus Karlsson <pajlada@bithack.se>
2015-06-18 00:43:48 +0000

    Magically bumped up version from 0.1.0 to 0.8.0

--- tyggbot.py
+++ tyggbot.py
@@ -28,7 +28,7 @@ class TyggBot(irc.client.SimpleIRCClient):
     Main class for the twitch bot
     """
 
-    version = '0.1.0'
+    version = '0.8.0'
     date_fmt = '%H:%M'
     #date_fmt = '%A %B '
     commands = {}
