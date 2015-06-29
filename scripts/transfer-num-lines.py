8cdf8e4040177408db99281a519d9d6d802abab0
Rasmus Karlsson <pajlada@bithack.se>
2015-06-29 10:57:38 +0000

    Added a script to transfer num-lines to the new user system

---
+++ scripts/transfer-num-lines.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import sys, os
+import configparser
+import pymysql
+
+sys.path.insert(0, os.path.dirname(os.path.realpath(__file__ + '/../')))
+os.chdir(os.path.dirname(os.path.realpath(__file__ + '/../')))
+
+from models.user import User, UserManager
+from kvidata import KVIData
+
+config = configparser.ConfigParser()
+
+config.read('config.ini')
+
+sqlconn = pymysql.connect(unix_socket=config['sql']['unix_socket'], user=config['sql']['user'], passwd=config['sql']['passwd'], db=config['sql']['db'], charset='utf8')
+kvi = KVIData(sqlconn)
+
+users = UserManager(sqlconn)
+
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+for nl in kvi.fetch_all('nl'):
+    user = users[nl['key']]
+    user.num_lines = nl['value']
+    user.needs_sync = True
+
+users.sync()
