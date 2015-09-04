0abb1e3fbea69b30b61d0ef578a0cebc2866bb47
Rasmus Karlsson <pajlada@bithack.se>
2015-09-04 01:54:14 +0000

    Version bumped up to 1.2.0

--- tyggbot.py
+++ tyggbot.py
@@ -53,7 +53,7 @@ class TyggBot:
     should never have two active classes. """
     instance = None
 
-    version = '1.1.0'
+    version = '1.2.0'
     date_fmt = '%H:%M'
     update_chatters_interval = 5
 
