a36e49f09dcfb5d04863466b5bbdfbe05841b3f4
Rasmus Karlsson <pajlada@bithack.se>
2015-10-11 23:29:54 +0000

    Improved the pyramid parser a bit.
    You can no longer create 2-width pyramids!

--- models/pyramidparser.py
+++ models/pyramidparser.py
@@ -6,6 +6,7 @@ log = logging.getLogger('tyggbot')
 
 class PyramidParser:
     data = []
+    going_down = False
     regex = re.compile(' +')
 
     def __init__(self, bot):
@@ -22,6 +23,8 @@ class PyramidParser:
                 len_diff = cur_len - last_len
                 if abs(len_diff) == 1:
                     good = True
+
+                    # Make sure the pyramid consists of the same item over and over again
                     for x in msg_parts:
                         if not x == pyramid_thing:
                             good = False
@@ -29,7 +32,12 @@ class PyramidParser:
 
                     if good:
                         self.data.append(msg_parts)
-                        if len_diff < 0:
+                        if len_diff > 0:
+                            if self.going_down:
+                                self.data = []
+                                self.going_down = False
+                        elif len_diff < 0:
+                            self.going_down = True
                             if cur_len == 1:
                                 # A pyramid was finished
                                 peak_length = 0
@@ -38,20 +46,26 @@ class PyramidParser:
                                         peak_length = len(x)
 
                                 response = 'PogChamp'
-                                if peak_length <= 2:
-                                    response = 'wow... ResidentSleeper //'
-                                elif peak_length < 5:
-                                    response = 'That\'s pretty neat KKona //'
-                                elif peak_length < 7:
-                                    response = 'Good job!!! PogChamp //'
-                                else:
-                                    response = 'Now that\'s what I call pyramid-farming Kappa // PogChamp // OSbury //'
-                                self.bot.say('{0} just finished a {1}-width {2} pyramid {3}'.format(source.username_raw, peak_length, pyramid_thing, response))
+                                if peak_length > 2:
+                                    if peak_length < 5:
+                                        response = 'That\'s pretty neat KKona //'
+                                    elif peak_length < 7:
+                                        response = 'Good job!!! PogChamp //'
+                                    elif peak_length < 15:
+                                        response = 'Now that\'s what I call pyramid-farming Kappa // PogChamp // OSbury //'
+                                    elif peak_length < 25:
+                                        response = 'Wow, I can\'t even fit this pyramid in my memory bank MrDestructoid //'
+                                    else:
+                                        response = 'BUFFER OVERFLOW WutFace //'
+                                    self.bot.say('{0} just finished a {1}-width {2} pyramid! {3}'.format(source.username_raw, peak_length, pyramid_thing, response))
                                 self.data = []
+                                self.going_down = False
                     else:
                         self.data = []
+                        self.going_down = False
                 else:
                     self.data = []
+                    self.going_down = False
 
             if len(msg_parts) == 1 and len(self.data) == 0:
                 self.data.append(msg_parts)
