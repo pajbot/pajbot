ec3c897574e87967d6f67c5ca55d4ed7844a99de
Rasmus Karlsson <pajlada@bithack.se>
2015-11-23 01:32:00 +0000

    You can now search for users.
    This is done through the input field in the top right corner of the
    website. This is visible on all pages that have the menu bar.

--- static/scripts/base.js
+++ static/scripts/base.js
@@ -1,3 +1,14 @@
 $.fn.api.settings.api = {
     'get_user': '/api/v1/user/{username}',
 };
+
+$(document).ready(function() {
+    $('#usersearch').form({
+        onSuccess: function(settings) {
+            console.log('hi');
+            console.log(settings);
+            document.location.href = '/user/'+$('#usersearch input.username').val();
+            return false;
+        }
+    });
+});
