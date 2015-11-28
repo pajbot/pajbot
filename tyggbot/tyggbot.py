f35f767f4832e1889b16677c1edfd3f09c1668c9
Rasmus Karlsson <pajlada@bithack.se>
2015-11-28 18:35:01 +0000

    Version update: 2.0.4 > 2.0.5
    420 hype CiGrip

--- tyggbot/tyggbot.py
+++ tyggbot/tyggbot.py
@@ -51,7 +51,7 @@ class TyggBot:
     Main class for the twitch bot
     """
 
-    version = '2.0.4'
+    version = '2.0.5'
     date_fmt = '%H:%M'
     update_chatters_interval = 5
     admin = None
