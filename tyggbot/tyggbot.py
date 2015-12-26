aa6f8d7fbef48820f020dbaed293aa9380c9b019
Rasmus Karlsson <pajlada@bithack.se>
2015-12-26 03:10:16 +0000

    Added another error message for if alembic fails.

--- tyggbot/tyggbot.py
+++ tyggbot/tyggbot.py
@@ -160,6 +160,10 @@ class TyggBot:
             log.exception('aaaa')
             log.error('Unable to call `alembic upgrade head`, this means the database could be out of date. Quitting.')
             sys.exit(1)
+        except PermissionError:
+            log.error('No permission to run `alembic upgrade head`. This means your user probably doesn\'t have execution rights on the `alembic` binary.')
+            log.error('The error can also occur if it can\'t find `alembic` in your PATH, and instead tries to execute the alembic folder.')
+            sys.exit(1)
         except FileNotFoundError:
             log.error('Could not found an installation of alembic. Please install alembic to continue.')
             sys.exit(1)
