d396d476116db2415f98bbe1999ce8e447c34b8e
Rasmus Karlsson <pajlada@bithack.se>
2015-12-27 00:27:07 +0000

    Converted the styling to sass.
    The sass is probably very ugly right now, and needs to be restructured.
    I'm not very good with frontend stuff, so if someone else has any
    suggestions, please let me know or just send me a pull request(hint hint).
    looks dank tho

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,23 @@
 var gulp = require('gulp');
 var watch = require('gulp-watch');
+var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require("gulp-rename");
 
+gulp.task('sass', function() {
+    gulp.src('./static/css/*.sass')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./static/css'));
+});
+
 gulp.task('minify', function () {
-  gulp.src('./static/css/base.css')
-  .pipe(minifyCss({compatibility: 'ie8'}))
-  .pipe(rename('base.min.css'))
-  .pipe(gulp.dest('./static/css/'))
+    gulp.src('./static/css/base.css')
+        .pipe(minifyCss({compatibility: 'ie8'}))
+        .pipe(rename('base.min.css'))
+        .pipe(gulp.dest('./static/css/'))
 });
 
 gulp.task('watch',function() {
-  gulp.watch('./static/css/base.css',['minify']);
+    gulp.watch('./static/css/*.sass', ['sass']);
+    gulp.watch('./static/css/*.css', ['minify']);
 });
