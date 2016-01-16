dddf482cd75bdecffb238802051e4a038e8531bc
Rasmus Karlsson <pajlada@bithack.se>
2016-01-16 00:32:10 +0000

    Removed point multiplier for plebs in pajlada's chat

--- pajbot/bot.py
+++ pajbot/bot.py
@@ -382,8 +382,6 @@ class Bot:
             else:
                 user.minutes_in_chat_offline += self.update_chatters_interval
             num_points = points
-            if user.subscriber or self.streamer == 'pajlada':
-                num_points *= 5
             if self.streamer == 'forsenlol' and 'trump_sub' in user.tags:
                 num_points *= 0.5
             user.touch(num_points)
