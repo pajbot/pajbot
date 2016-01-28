e804434cb650d568a15ff233224b32cda45442a6
Rasmus Karlsson <pajlada@bithack.se>
2016-01-28 20:43:33 +0000

    Fixed a crash issue in multiraffles.
    It occured if a multiraffle was started for less than 100 points.

--- pajbot/modules/multiraffle.py
+++ pajbot/modules/multiraffle.py
@@ -114,7 +114,7 @@ class MultiRaffleModule(BaseModule):
                 points_per_user = math.ceil(abs_points / num_winners)
                 break
             elif points_per_user < min_point_award:
-                num_winners = min(math.floor(abs_points / min_point_award), num_participants)
+                num_winners = max(1, min(math.floor(abs_points / min_point_award), num_participants))
                 points_per_user = math.ceil(abs_points / num_winners)
                 break
 
