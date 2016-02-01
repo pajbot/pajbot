0952aa5c443548075f363ff4b5fd7a04055d2e1c
Rasmus Karlsson <pajlada@bithack.se>
2016-02-01 23:11:15 +0000

    Completed a quest! TypeEmote quest thing ;-)

--- pajbot/modules/quests/base.py
+++ pajbot/modules/quests/base.py
@@ -1,16 +1,24 @@
 import logging
 
 from pajbot.modules import BaseModule, ModuleSetting
+from pajbot.streamhelper import StreamHelper
 
 log = logging.getLogger(__name__)
 
 class BaseQuest(BaseModule):
     OBJECTIVE = 'No objective set.'
 
+    def __init__(self):
+        super().__init__()
+        self.progress_key = '{streamer}:current_quest_progress'.format(streamer=StreamHelper.get_streamer())
+
     def start_quest(self):
+        """ This method is triggered by either the stream starting, or the bot loading up
+        while a quest/stream is already active """
         log.error('No start quest implemented for this quest.')
 
     def stop_quest(self):
+        """ This method is ONLY called when the stream is stopped. """
         log.error('No stop quest implemented for this quest.')
 
     def get_objective(self):
--- pajbot/modules/quests/typeemote.py
+++ pajbot/modules/quests/typeemote.py
@@ -3,33 +3,83 @@ import logging
 from pajbot.modules import BaseModule, ModuleSetting
 from pajbot.modules import QuestModule
 from pajbot.modules.quests import BaseQuest
+from pajbot.managers import RedisManager
 from pajbot.models.handler import HandlerManager
 from pajbot.models.command import Command
+
+from numpy import random
 
 log = logging.getLogger(__name__)
 
 class TypeEmoteQuestModule(BaseQuest):
 
     ID = 'quest-' + __name__.split('.')[-1]
-    NAME = 'Type X emote Y times (WIP)'
+    NAME = 'Type X emote Y times'
     DESCRIPTION = 'A user needs to type a specific emote Y times to complete this quest.'
     PARENT_MODULE = QuestModule
 
-    PROGRESS = 1
+    PROGRESS = 40
     LIMIT = 1
     REWARD = 3
 
+    def __init__(self):
+        super().__init__()
+        self.current_emote_key = '{streamer}:current_quest_emote'
+        self.current_emote = '???'
+        self.progress = {}
+
     def on_message(self, source, message, emotes, whisper, urls):
-        pass
+        for emote in emotes:
+            if emote['code'] == self.current_emote:
+                if source.username in self.progress:
+                    user_progress = self.progress[source.username] + 1
+                else:
+                    user_progress = 1
+
+                if user_progress > self.PROGRESS:
+                    # no need to do more
+                    return
+
+                self.progress[source.username] = user_progress
+
+                redis = RedisManager.get()
+
+                if user_progress == self.PROGRESS:
+                    source.award_tokens(self.REWARD, redis=redis)
+
+                redis.hset(self.progress_key, source.username, user_progress)
+                return
 
     def start_quest(self):
         HandlerManager.add_handler('on_message', self.on_message)
 
+        redis = RedisManager.get()
+
+        old_progress = redis.hgetall(self.progress_key)
+        for user, progress in old_progress.items():
+            try:
+                self.progress[user.decode('utf8')] = int(progress)
+            except (TypeError, ValueError):
+                pass
+        self.current_emote = redis.get(self.current_emote_key)
+        if self.current_emote is None:
+            # randomize an emote
+            global_twitch_emotes = self.bot.emotes.get_global_emotes()
+            self.current_emote = random.choice(global_twitch_emotes)
+            redis.set(self.current_emote_key, self.current_emote)
+        else:
+            self.current_emote = self.current_emote.decode('utf8')
+
     def stop_quest(self):
         HandlerManager.remove_handler('on_message', self.on_message)
 
+        redis = RedisManager.get()
+
+        redis.delete(self.progress_key)
+        redis.delete(self.current_emote_key)
+
     def get_objective(self):
-        return 'asd'
+        return 'Use the {} emote {} times'.format(self.current_emote, self.PROGRESS)
 
     def enable(self, bot):
         self.bot = bot
