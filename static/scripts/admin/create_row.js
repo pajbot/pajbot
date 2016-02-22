a87a7a3db7dc2528c020e7ca8c5f502f8421e12f
Rasmus Karlsson <pajlada@bithack.se>
2016-02-22 23:49:16 +0000

    Changed the enable/disable button in the admin interface.
    It's now a checkbox (kind of ) instead of a regular button that changes
    state from Enable/Disable.
    This should make it easier to see whether a module or enabled or
    disabled.
    
    Fixes #181

--- static/scripts/admin/create_row.js
+++ static/scripts/admin/create_row.js
@@ -63,3 +63,36 @@ function enable_toggle_row(action)
         },
     });
 }
+
+function enable_toggle_row_cb(action)
+{
+    $('.toggle-row').api({
+        on: 'change',
+        action: action,
+        method: 'post',
+        successTest: function(response) {
+            return response.success || false;
+        },
+        beforeSend: function(settings) {
+            var state = $(this).parent().parent().parent().data('enabled');
+            console.log(state);
+            settings.urlData.id = $(this).parent().parent().parent().data('id');
+            if (state == '1') {
+                settings.data.new_state = 0;
+            } else {
+                settings.data.new_state = 1;
+            }
+            return settings;
+        },
+        onSuccess: function(response, element) {
+            $(this).parent().parent().parent().data('enabled', response.new_state);
+            if (response.new_state == 1) {
+                $(element).find('.text').text('Disable');
+                $(element).find('.icon').removeClass('green').addClass('red');
+            } else {
+                $(element).find('.text').text('Enable');
+                $(element).find('.icon').removeClass('red').addClass('green');
+            }
+        },
+    });
+}
