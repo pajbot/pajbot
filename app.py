70ef2e68d6777425d3fd3f3f311d8c27e700321f
Rasmus Karlsson <pajlada@bithack.se>
2016-03-07 01:16:29 +0000

    Moved /highlights/ to its own base file

--- app.py
+++ app.py
@@ -36,8 +36,7 @@ from flask import url_for
 from flask.ext.scrypt import generate_random_salt
 from flask_oauthlib.client import OAuth
 from flask_oauthlib.client import OAuthException
-from sqlalchemy import func, cast, Date
-from sqlalchemy.orm import joinedload
+from sqlalchemy import func
 import markdown
 
 init_logging('pajbot')
@@ -237,62 +236,6 @@ def debug():
 def contact():
     return render_template('contact.html')
 
-@app.route('/highlights/<date>/')
-def highlight_list_date(date):
-    # Make sure we were passed a valid date
-    try:
-        parsed_date = datetime.datetime.strptime(date, '%Y-%m-%d')
-    except ValueError:
-        # Invalid date
-        return redirect('/highlights/', 303)
-    session = DBManager.create_session()
-    dates_with_highlights = []
-    highlights = session.query(StreamChunkHighlight).options(joinedload('*')).filter(cast(StreamChunkHighlight.created_at, Date) == parsed_date).order_by(StreamChunkHighlight.created_at.desc()).all()
-    for highlight in session.query(StreamChunkHighlight):
-        dates_with_highlights.append(datetime.datetime(
-            year=highlight.created_at.year,
-            month=highlight.created_at.month,
-            day=highlight.created_at.day))
-
-    try:
-        return render_template('highlights_date.html',
-                highlights=highlights,
-                date=parsed_date,
-                dates_with_highlights=set(dates_with_highlights))
-    finally:
-        session.close()
-
-@app.route('/highlights/<date>/<highlight_id>', defaults={'highlight_title': None})
-@app.route('/highlights/<date>/<highlight_id>-<highlight_title>')
-def highlight_id(date, highlight_id, highlight_title=None):
-    with DBManager.create_session_scope() as db_session:
-        highlight = db_session.query(StreamChunkHighlight).options(joinedload('*')).filter_by(id=highlight_id).first()
-        if highlight is None:
-            return render_template('highlight_404.html'), 404
-        else:
-            stream_chunk = highlight.stream_chunk
-            stream = stream_chunk.stream
-        return render_template('highlight.html',
-                highlight=highlight,
-                stream_chunk=stream_chunk,
-                stream=stream)
-
-@app.route('/highlights/')
-def highlights():
-    session = DBManager.create_session()
-    dates_with_highlights = []
-    highlights = session.query(StreamChunkHighlight).options(joinedload('*')).order_by(StreamChunkHighlight.created_at_with_offset.desc()).all()
-    for highlight in highlights:
-        dates_with_highlights.append(datetime.datetime(
-            year=highlight.created_at.year,
-            month=highlight.created_at.month,
-            day=highlight.created_at.day))
-    try:
-        return render_template('highlights.html',
-                highlights=highlights[:10],
-                dates_with_highlights=set(dates_with_highlights))
-    finally:
-        session.close()
 
 @app.route('/notifications/')
 def notifications():
--- pajbot/web/routes/base/__init__.py
+++ pajbot/web/routes/base/__init__.py
@@ -1,5 +1,6 @@
 import pajbot.web.routes.base.commands
 import pajbot.web.routes.base.decks
+import pajbot.web.routes.base.highlights
 import pajbot.web.routes.base.home
 import pajbot.web.routes.base.pleblist
 import pajbot.web.routes.base.stats
@@ -7,6 +8,6 @@ import pajbot.web.routes.base.stats
 def init(app):
     pajbot.web.routes.base.commands.init(app)
     pajbot.web.routes.base.decks.init(app)
+    pajbot.web.routes.base.highlights.init(app)
     pajbot.web.routes.base.home.init(app)
     pajbot.web.routes.base.pleblist.init(app)
-    pajbot.web.routes.base.stats.init(app)
