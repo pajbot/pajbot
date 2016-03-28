c55ca2b0e8fe0c6ad5d266e1beb3aec6dd70cd1a
Rasmus Karlsson <pajlada@bithack.se>
2016-03-28 22:05:09 +0000

    Updated the startbot script to make sure the locale is properly set.
    Idk if this is actually a good fix or not, but it fixed my issue!

--- startbot.sh
+++ startbot.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-PATH="`pwd`/myvenv/bin:$PATH" /usr/bin/pm2 start main.py --name="$1" --force --output="/var/log/pajbot/$1.log" --error="/var/log/pajbot/$1.err" --merge-logs -- --config configs/$1.ini
+export LANG="en_US.utf8"
+
+LANG="en_US.utf8" PATH="`pwd`/myvenv/bin:$PATH" /usr/bin/pm2 start main.py --name="$1" --force --output="/var/log/pajbot/$1.log" --error="/var/log/pajbot/$1.err" --merge-logs -- --config configs/$1.ini
