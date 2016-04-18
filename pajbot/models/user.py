e579c28deb9b6925935fff0d0f7910372bcdf6cb
rigor789 <rigor789@gmail.com>
2016-04-18 17:36:18 +0100

    Keep the import in, so alembic doesn't sqeak when migrating.

--- pajbot/models/user.py
+++ pajbot/models/user.py
@@ -12,12 +12,13 @@ from sqlalchemy import String
 from sqlalchemy.orm import reconstructor
 from sqlalchemy.orm import relationship
 
-
 from pajbot.managers import Base
 from pajbot.managers import DBManager
 from pajbot.managers import HandlerManager
 from pajbot.managers import RedisManager
 from pajbot.managers import TimeManager
+
+from pajbot.models.apitoken import APIToken  # NOQA
 
 from pajbot.streamhelper import StreamHelper
 
