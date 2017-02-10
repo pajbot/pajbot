e677866e20276bb55b6eeb39c3e807ab5d34cde2
Rasmus Karlsson <pajlada@bithack.se>
2017-02-10 13:26:32 +0000

    update how we handle assets.
    we no longer rely on the user building semantic ui themselves, instead
    we use a cloud-hosted version

--- pajbot/web/common/assets.py
+++ pajbot/web/common/assets.py
@@ -10,58 +10,55 @@ def init(app):
     assets = Environment(app)
 
     # Basic CSS and Javascript:
-    # Available under: base_css, semantic_css, base_js
-    base_css = Bundle('css/base.min.css',
-            output='css/base.gen.%(version)s.css')
-    semantic_css = Bundle('semantic/semantic.min.css',
-            output='semantic/semantic.gen.%(version)s.css')
+    # Available under: base_css, base_js
+    base_css = Bundle('css/base.scss', filters='pyscss',
+            output='gen/css/base.%(version)s.css')
     base_js = Bundle('scripts/base.js', filters='jsmin',
-            output='scripts/base.gen.%(version)s.js')
-    semantic_js = Bundle('semantic/semantic.min.js',
-            output='semantic/semantic.gen.%(version)s.js')
+            output='gen/scripts/base.%(version)s.js')
     assets.register('base_css', base_css)
     assets.register('base_js', base_js)
-    assets.register('semantic_css', semantic_css)
-    assets.register('semantic_js', semantic_js)
 
     # Pleblist-related javascript
-    # Available undeer the following assets: pleblist_shared, pleblist_host, pleblist_client
+    # Available under the following assets: pleblist_shared, pleblist_host, pleblist_client
     pleblist_client = Bundle('scripts/pleblist.js', filters='jsmin',
-            output='scripts/pleblist.gen.%(version)s.js')
+            output='gen/scripts/pleblist.%(version)s.js')
     pleblist_shared = Bundle('scripts/pleblist.shared.js', filters='jsmin',
-            output='scripts/pleblist.gen.shared.%(version)s.js')
+            output='gen/scripts/pleblist.shared.%(version)s.js')
     pleblist_host = Bundle('scripts/pleblist.host.js', filters='jsmin',
-            output='scripts/pleblist.gen.host.%(version)s.js')
+            output='gen/scripts/pleblist.host.%(version)s.js')
     assets.register('pleblist_shared', pleblist_shared)
     assets.register('pleblist_client', pleblist_client)
     assets.register('pleblist_host', pleblist_host)
 
     # CLR Overlay
-    # Availabe under: clr_overlay, clr_donations, clr_shared
+    # Availabe under: clr_overlay_js, clr_overlay_css, clr_donations_js, clr_donations_css, clr_shared_js
     clr_overlay_js = Bundle('scripts/clr.overlay.js', filters='jsmin',
-            output='scripts/clr.gen.overlay.%(version)s.js')
-    clr_overlay_css = Bundle('css/clr.overlay.css',
-            output='css/gen.clr.overlay.%(version)s.css')
-    clr_donations = Bundle('scripts/clr.donations.js', filters='jsmin',
-            output='scripts/clr.gen.donations.%(version)s.js')
-    clr_shared = Bundle('scripts/clr.shared.js', filters='jsmin',
-            output='scripts/clr.gen.shared.%(version)s.js')
+            output='gen/scripts/clr.overlay.%(version)s.js')
+    clr_overlay_css = Bundle('css/clr.overlay.scss', filters='pyscss',
+            output='gen/css/clr.overlay.%(version)s.css')
+    clr_donations_js = Bundle('scripts/clr.donations.js', filters='jsmin',
+            output='gen/scripts/clr.donations.%(version)s.js')
+    clr_donations_css = Bundle('css/clr.donations.scss', filters='pyscss',
+            output='gen/css/clr.donations.%(version)s.css')
+    clr_shared_js = Bundle('scripts/clr.shared.js', filters='jsmin',
+            output='gen/scripts/clr.shared.%(version)s.js')
     assets.register('clr_overlay_js', clr_overlay_js)
     assets.register('clr_overlay_css', clr_overlay_css)
-    assets.register('clr_donations', clr_donations)
-    assets.register('clr_shared', clr_shared)
+    assets.register('clr_donations_js', clr_donations_js)
+    assets.register('clr_donations_css', clr_donations_css)
+    assets.register('clr_shared_js', clr_shared_js)
 
     # Admin site
     # Availabe under: admin_create_banphrase, admin_create_command,
     #                 admin_create_row, admin_edit_command
     admin_create_banphrase = Bundle('scripts/admin/create_banphrase.js', filters='jsmin',
-            output='scripts/admin/create_banphrase.gen.%(version)s.js')
+            output='gen/scripts/admin/create_banphrase.%(version)s.js')
     admin_create_command = Bundle('scripts/admin/create_command.js', filters='jsmin',
-            output='scripts/admin/create_command.gen.%(version)s.js')
+            output='gen/scripts/admin/create_command.%(version)s.js')
     admin_create_row = Bundle('scripts/admin/create_row.js', filters='jsmin',
-            output='scripts/admin/create_row.gen.%(version)s.js')
+            output='gen/scripts/admin/create_row.%(version)s.js')
     admin_edit_command = Bundle('scripts/admin/edit_command.js', filters='jsmin',
-            output='scripts/admin/edit_command.gen.%(version)s.js')
+            output='gen/scripts/admin/edit_command.%(version)s.js')
     assets.register('admin_create_banphrase', admin_create_banphrase)
     assets.register('admin_create_command', admin_create_command)
     assets.register('admin_create_row', admin_create_row)
@@ -69,33 +66,33 @@ def init(app):
 
     # Admin CLR
     admin_clr_donations_edit_js = Bundle('scripts/admin/clr/donations/edit.js',
-            output='scripts/admin/clr/donations/gen.edit.%(version)s.js')
+            output='gen/scripts/admin/clr/donations/edit.%(version)s.js')
     assets.register('admin_clr_donations_edit_js', admin_clr_donations_edit_js)
 
     notifications_base = Bundle('scripts/notifications/base.js', filters='jsmin',
-            output='scripts/notifications/base.gen.%(version)s.js')
+            output='gen/scripts/notifications/base.%(version)s.js')
     assets.register('notifications_base', notifications_base)
 
     notifications_subscribers = Bundle('scripts/notifications/subscribers.js', filters='jsmin',
-            output='scripts/notifications/subscribers.gen.%(version)s.js')
+            output='gen/scripts/notifications/subscribers.%(version)s.js')
     assets.register('notifications_subscribers', notifications_subscribers)
 
     # Third party libraries
     # Available under: autolinker
     autolinker = Bundle('scripts/autolinker.js', filters='jsmin',
-            output='scripts/autolinker.gen.%(version)s.js')
+            output='gen/scripts/autolinker.%(version)s.js')
     assets.register('autolinker', autolinker)
 
     # Commands
     # Available under: commands_js
     commands_js = Bundle('scripts/commands.js', filters='jsmin',
-            output='scripts/gen.commands.%(version)s.js')
+            output='gen/scripts/commands.%(version)s.js')
     assets.register('commands_js', commands_js)
 
     # Pagination script
     # Available under: paginate_js
     paginate_js = Bundle('scripts/paginate.js', filters='jsmin',
-            output='scripts/gen.paginate.%(version)s.js')
+            output='gen/scripts/paginate.%(version)s.js')
     assets.register('paginate_js', paginate_js)
 
     assets.init_app(app)
