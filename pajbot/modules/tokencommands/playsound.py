73fb019133c1672e2b3a5a3a33228a94048e73b1
Rasmus Karlsson <pajlada@bithack.se>
2018-06-15 07:06:58 +0000

    Change how playsounds are listed, also update playsounds

--- pajbot/modules/tokencommands/playsound.py
+++ pajbot/modules/tokencommands/playsound.py
@@ -10,133 +10,135 @@ from pajbot.modules import QuestModule
 log = logging.getLogger(__name__)
 
 
-class Sample:
-    def __init__(self, command, *links, new=False):
-        self.command = command
-        self.links = links
-        self.href = links[0]
-        self.new = new
-
-    def __lt__(self, other):
-        return self.command < other.command
-
-
 class Samples:
-    all_samples = [
-            Sample('bossofthisgym', 'http://soundboard.ass-we-can.com/static/music/MarkW/Boss of this gym.mp3'),
-            Sample('fuckyou', 'http://soundboard.ass-we-can.com/static/music/VanD/FUCKYOU.mp3'),
-            Sample('idontdoanal', 'https://pajlada.se/files/clr/i_dont_do_anal.mp3'),
-            Sample('knock', 'https://pajlada.se/files/clr/knock.mp3'),
-            Sample('slap', 'https://pajlada.se/files/clr/slap.mp3'),
-            Sample('cumming', 'https://pajlada.se/files/clr/cumming.mp3'),
-            Sample('collegeboy', 'http://soundboard.ass-we-can.com/static/music/BillyH/Come%20on%20college%20boy.mp3'),
-            Sample('oooh', 'https://pajlada.se/files/clr/oooh.mp3'),
-            Sample('suction', 'https://pajlada.se/files/clr/suction.mp3'),
-            Sample('takeit', 'http://soundboard.ass-we-can.com/static/music/VanD/Take%20it%20boy.mp3'),
-            Sample('amazing', 'http://soundboard.ass-we-can.com/static/music/VanD/That\'s%20amazing.mp3'),
-            Sample('power', 'https://pajlada.se/files/clr/power.mp3'),
-            Sample('othernight', 'https://pajlada.se/files/clr/othernight.mp3'),
-            Sample('asswecan', 'https://pajlada.se/files/clr/ass_we_can.mp3'),
-            Sample('lashofthespanking', 'http://soundboard.ass-we-can.com/static/music/BillyH/Lash%20of%20the%20spanking.mp3'),
-            Sample('nyanpass', 'https://pajlada.se/files/clr/nyanpass.mp3'),
-            Sample('scamazishere', 'https://pajlada.se/files/clr/scamaz_is_here.mp3'),
-            Sample('lul', 'https://pajlada.se/files/clr/LUL.mp3'),
-            Sample('ohmyshoulder', 'https://pajlada.se/files/clr/ohmyshoulder.mp3'),
-            Sample('tuturu', 'https://pajlada.se/files/clr/tuturu.mp3'),
-            Sample('attention', 'https://pajlada.se/files/clr/attention.mp3'),
-            Sample('aaaah', 'https://pajlada.se/files/clr/AAAAAAAH.mp3'),
-            Sample('jesse', 'https://pajlada.se/files/clr/jesse-cook.mp3'),
-            Sample('shakalaka', 'https://pajlada.se/files/clr/shakalaka.mp3'),
-            Sample('loan', 'https://pajlada.se/files/clr/its_a_loan.mp3'),
-            Sample('spankmoan1', 'https://pajlada.se/files/clr/spankmoan1.mp3'),
-            Sample('youlikechallenges', 'https://pajlada.se/files/clr/you_like_challenges.mp3'),
-            Sample('youlikethat', 'https://pajlada.se/files/clr/you_like_that.mp3'),
-            Sample('pants', 'https://pajlada.se/files/clr/ripped_pants.mp3'),
-            Sample('oh', 'https://pajlada.se/files/clr/oh.mp3'),
-            Sample('poi', 'https://pajlada.se/files/clr/poi.mp3'),
-            Sample('ayaya', 'https://pajlada.se/files/clr/ayaya.mp3'),
-            Sample('car', 'https://pajlada.se/files/clr/car.mp3'),
-            Sample('dayum', 'https://pajlada.se/files/clr/dayum.mp3'),
-            Sample('water', 'https://pajlada.se/files/clr/water1.mp3'),
-            Sample('doitdad', 'https://pajlada.se/files/clr/do_it_dad.mp3'),
-            Sample('face', 'https://pajlada.se/files/clr/me_go_face.mp3'),
-            Sample('sike', 'https://pajlada.se/files/clr/sike.mp3'),
-            Sample('yahallo', 'https://pajlada.se/files/clr/yahallo.mp3'),
-            Sample('djkarlthedog', 'https://pajlada.se/files/clr/djkarlthedog.mp3'),
-            Sample('bomblobber', 'https://pajlada.se/files/clr/bomb_lobber.mp3'),
-            Sample('baka', 'https://pajlada.se/files/clr/baka.mp3'),
-            Sample('march', 'https://pajlada.se/files/clr/march.mp3'),
-            Sample('embarrassing', 'https://pajlada.se/files/clr/embarrassing.mp3'),
-            Sample('yessir', 'https://pajlada.se/files/clr/yes_sir.mp3'),
-            Sample('sixhotloads', 'https://pajlada.se/files/clr/six_hot_loads.mp3'),
-            Sample('wrongnumba', 'https://pajlada.se/files/clr/wrong_numba.mp3'),
-            Sample('sorry', 'https://pajlada.se/files/clr/sorry.mp3'),
-            Sample('relax', 'https://pajlada.se/files/clr/relax.mp3'),
-            Sample('vibrate', 'https://pajlada.se/files/clr/vibrate.mp3'),
+    valid_samples = {
+            '4head': { 'length': 0 },
+            '4header': { 'length': 0 },
+            '7777': { 'length': 0 },
+            'aaaah': { 'length': 0 },
+            'actioniscoming': { 'length': 0 },
+            'akarin': { 'length': 0 },
+            'amazing': { 'length': 0 },
+            'asswecan': { 'length': 0 },
+            'athene': { 'length': 0 },
+            'attention': { 'length': 0 },
+            'ayaya': { 'length': 0 },
+            'baka': { 'length': 0 },
+            'beatme123': { 'length': 0 },
+            'behindyou': { 'length': 0 },
+            'bitch': { 'length': 0 },
+            'bomblobber': { 'length': 0 },
+            'bondagegaywebsite': { 'length': 0 },
+            'bossofthisgym': { 'length': 0 },
+            'boyishgiggles': { 'length': 0 },
+            'bruceuiscoming': { 'length': 0 },
+            'bubble': { 'length': 0 },
+            'car': { 'length': 0 },
+            'celebrate': { 'length': 0 },
+            'collegeboy': { 'length': 0 },
+            'comeonletsgo': { 'length': 0 },
+            'cumming': { 'length': 0 },
+            'damnson': { 'length': 0 },
+            'dayum': { 'length': 0 },
+            'deadlycommandos': { 'length': 0 },
+            'desu': { 'length': 0 },
+            'djkarlthedog': { 'length': 0 },
+            'doitdad': { 'length': 0 },
+            'doot': { 'length': 0 },
+            'eatthepoopoo': { 'length': 0 },
+            'embarrassing': { 'length': 0 },
+            'eshrug': { 'length': 0 },
+            'face': { 'length': 0 },
+            'fatcock': { 'length': 0 },
+            'forsenswa': { 'length': 0 },
+            'fuckyou': { 'length': 0 },
+            'gamba': { 'length': 0 },
+            'gangingup': { 'length': 0 },
+            'goodvibes': { 'length': 0 },
+            'heftobemad': { 'length': 0 },
+            'heyguyshowsitgoinkripparrianhere': { 'length': 0 },
+            'howstrong': { 'length': 0 },
+            'hyperbruh': { 'length': 0 },
+            'idontdoanal': { 'length': 0 },
+            'iseeyou1': { 'length': 0 },
+            'iseeyou2': { 'length': 0 },
+            'jabroni': { 'length': 0 },
+            'jeff': { 'length': 0 },
+            'jesse': { 'length': 0 },
+            'knock': { 'length': 0 },
+            'lashofthespanking': { 'length': 0 },
+            'legendary': { 'length': 0 },
+            'levelup': { 'length': 0 },
+            'loan': { 'length': 0 },
+            'lul': { 'length': 0 },
+            'march': { 'length': 0 },
+            'mistake': { 'length': 0 },
+            'mysummercmonman': { 'length': 0 },
+            'nani': { 'length': 0 },
+            'no': { 'length': 0 },
+            'nothinghere': { 'length': 0 },
+            'nyanpass': { 'length': 0 },
+            'oh': { 'length': 0 },
+            'ohbabyatriple': { 'length': 0 },
+            'ohmancmonman': { 'length': 0 },
+            'ohmyshoulder': { 'length': 0 },
+            'oooh': { 'length': 0 },
+            'oooooh': { 'length': 0 },
+            'othernight': { 'length': 0 },
+            'pain1': { 'length': 0 },
+            'pants': { 'length': 0 },
+            'pewdiepie': { 'length': 0 },
+            'pleaseno': { 'length': 0 },
+            'poi': { 'length': 0 },
+            'poopooiscoming': { 'length': 0 },
+            'power': { 'length': 0 },
+            'powerfuck': { 'length': 0 },
+            'pphop': { 'length': 0 },
+            'puke': { 'length': 0 },
+            'pullupourpants': { 'length': 0 },
+            'realtrapshit': { 'length': 0 },
+            'relax': { 'length': 0 },
+            'reynad': { 'length': 0 },
+            'righthappy': { 'length': 0 },
+            'rin': { 'length': 0 },
+            'ryuu': { 'length': 0 },
+            'scamazishere': { 'length': 0 },
+            'shakalaka': { 'length': 0 },
+            'sheeeit': { 'length': 0 },
+            'sike': { 'length': 0 },
+            'sixhotloads': { 'length': 0 },
+            'slap': { 'length': 0 },
+            'smartass': { 'length': 0 },
+            'sorry': { 'length': 0 },
+            'spankmoan1': { 'length': 0 },
+            'specimen': { 'length': 0 },
+            'spook': { 'length': 0 },
+            'suction': { 'length': 0 },
+            'surprise': { 'length': 0 },
+            'takeit': { 'length': 0 },
+            'ting1': { 'length': 0 },
+            'ting2': { 'length': 0 },
+            'ting3': { 'length': 0 },
+            'tuckfrump': { 'length': 0 },
+            'tuturu': { 'length': 0 },
+            'uguu': { 'length': 0 },
+            'ultralul': { 'length': 0 },
+            'umad': { 'length': 0 },
+            'vibrate': { 'length': 0 },
+            'water': { 'length': 0 },
+            'weed': { 'length': 0 },
+            'woah': { 'length': 0 },
+            'woop': { 'length': 0 },
+            'wrongdoor': { 'length': 0 },
+            'wrongnumba': { 'length': 0 },
+            'yahallo': { 'length': 0 },
+            'yaranaika': { 'length': 0 },
+            'yeehaw': { 'length': 0 },
+            'yessir': { 'length': 0 },
+            'youlikechallenges': { 'length': 0 },
+            'youlikethat': { 'length': 0 },
+        }
 
-            Sample('4head', 'https://pajlada.se/files/clr/4Head.mp3'),
-            Sample('akarin', 'https://pajlada.se/files/clr/akarin.mp3'),
-            Sample('behindyou', 'https://pajlada.se/files/clr/behindyou.mp3'),
-            Sample('bitch', 'https://pajlada.se/files/clr/bitch.mp3'),
-            Sample('damnson', 'https://pajlada.se/files/clr/damnson.mp3'),
-            Sample('desu', 'https://pajlada.se/files/clr/desu.mp3'),
-            Sample('fatcock', 'https://pajlada.se/files/clr/fatcock.mp3'),
-            Sample('gangingup', 'https://pajlada.se/files/clr/gangingup.mp3'),
-            Sample('iseeyou1', 'https://pajlada.se/files/clr/iseeyou1.mp3'),
-            Sample('iseeyou2', 'https://pajlada.se/files/clr/iseeyou2.mp3'),
-            Sample('jeff', 'https://pajlada.se/files/clr/jeff.mp3'),
-            Sample('mistake', 'https://pajlada.se/files/clr/mistake.mp3'),
-            Sample('ohbabyatriple', 'https://pajlada.se/files/clr/ohbabyatriple.mp3'),
-            Sample('rin', 'https://pajlada.se/files/clr/rin.mp3'),
-            Sample('sheeeit', 'https://pajlada.se/files/clr/sheeeit.mp3'),
-            Sample('spook', 'https://pajlada.se/files/clr/spook.mp3'),
-            Sample('surprise', 'https://pajlada.se/files/clr/surprise.mp3'),
-            Sample('tuckfrump', 'https://pajlada.se/files/clr/tuckfrump.mp3'),
-            Sample('uguu', 'https://pajlada.se/files/clr/uguu.mp3'),
-            Sample('weed', 'https://pajlada.se/files/clr/weed.mp3'),
-            Sample('wrongdoor', 'https://pajlada.se/files/clr/wrongdoor.mp3'),
-
-            Sample('ryuu', 'https://pajlada.se/files/clr/hanzo.mp3'),
-
-            Sample('yeehaw', 'https://pajlada.se/files/clr/yeehaw.mp3'),
-
-            Sample('4header', 'https://pajlada.se/files/clr/4header.ogg'),
-            Sample('athene', 'https://pajlada.se/files/clr/athene.ogg'),
-            Sample('beatme123', 'https://pajlada.se/files/clr/beatme123.ogg'),
-            Sample('bondagegaywebsite', 'https://pajlada.se/files/clr/bondagegaywebsite.ogg'),
-            Sample('bubble', 'https://pajlada.se/files/clr/bubble.ogg'),
-            Sample('celebrate', 'https://pajlada.se/files/clr/celebrate.ogg'),
-            Sample('comeonletsgo', 'https://pajlada.se/files/clr/comeonletsgo.ogg'),
-            Sample('gamba', 'https://pajlada.se/files/clr/gamba.ogg'),
-            Sample('goodvibes', 'https://pajlada.se/files/clr/goodvibes.ogg'),
-            Sample('heftobemad', 'https://pajlada.se/files/clr/heftobemad.ogg'),
-            Sample('heyguyshowsitgoinkripparrianhere', 'https://pajlada.se/files/clr/heyguyshowsitgoinkripparrianhere.ogg'),
-            Sample('jabroni', 'https://pajlada.se/files/clr/jabroni.ogg'),
-            Sample('legendary', 'https://pajlada.se/files/clr/legendary.ogg'),
-            Sample('mysummercmonman', 'https://pajlada.se/files/clr/mysummercmonman.ogg'),
-            Sample('no', 'https://pajlada.se/files/clr/no.ogg'),
-            Sample('nothinghere', 'https://pajlada.se/files/clr/nothinghere.ogg'),
-            Sample('ohmancmonman', 'https://pajlada.se/files/clr/ohmancmonman.ogg'),
-            Sample('oooooh', 'https://pajlada.se/files/clr/oooooh.ogg'),
-            Sample('pain1', 'https://pajlada.se/files/clr/pain1.ogg'),
-            Sample('pullupourpants', 'https://pajlada.se/files/clr/pants.ogg'),
-            Sample('pleaseno', 'https://pajlada.se/files/clr/pleaseno.ogg'),
-            Sample('powerfuck', 'https://pajlada.se/files/clr/powerfuck.ogg'),
-            Sample('puke', 'https://pajlada.se/files/clr/puke.ogg'),
-            Sample('reynad', 'https://pajlada.se/files/clr/reynad.ogg'),
-            Sample('righthappy', 'https://pajlada.se/files/clr/righthappy.ogg'),
-            Sample('smartass', 'https://pajlada.se/files/clr/smartass.ogg'),
-            Sample('ultralul', 'https://pajlada.se/files/clr/ultralul.ogg'),
-            Sample('yaranaika', 'https://pajlada.se/files/clr/yaranaika.ogg'),
-
-            Sample('woah', 'https://pajlada.se/files/clr/woah.ogg'),
-            Sample('realtrapshit', 'https://pajlada.se/files/clr/real-trap-shit.ogg'),
-
-            Sample('actioniscoming', 'https://pajlada.se/files/clr/action-is-coming.ogg', new=True),
-            Sample('ting1', 'https://pajlada.se/files/clr/ting-1.ogg', new=True),
-            Sample('ting2', 'https://pajlada.se/files/clr/ting-2.ogg', new=True),
-            Sample('ting3', 'https://pajlada.se/files/clr/ting-3.ogg', new=True),
-    ]
 
 
 class PlaySoundTokenCommandModule(BaseModule):
@@ -189,8 +191,7 @@ class PlaySoundTokenCommandModule(BaseModule):
 
     def __init__(self):
         super().__init__()
-        Samples.all_samples.sort()
-        self.valid_samples = [sample.command for sample in Samples.all_samples]
+        self.valid_samples = Samples.valid_samples
         self.sample_cache = []
 
     def play_sound(self, **options):
@@ -202,21 +203,22 @@ class PlaySoundTokenCommandModule(BaseModule):
             sample = message.split(' ')[0].lower()
 
             if sample in self.sample_cache:
+                bot.whisper(source.username, 'The sample {0} was played too recently. Please wait before trying to use it again'.format(sample))
                 return False
 
             if sample == 'random':
-                sample = random.choice(self.valid_samples)
+                sample = random.choice(self.valid_samples.keys())
 
             if sample in self.valid_samples:
                 log.debug('Played sound: {0}'.format(sample))
                 payload = {'sample': sample}
                 bot.websocket_manager.emit('play_sound', payload)
-                bot.whisper(source.username, 'Successfully played your sample {0}'.format(sample))
-                self.sample_cache.append(sample)
-                bot.execute_delayed(self.settings['sample_cd'], self.sample_cache.remove, ('{0}'.format(sample), ))
+                if not (source.username == 'pajlada') or True:
+                    self.sample_cache.append(sample)
+                    bot.execute_delayed(self.settings['sample_cd'], self.sample_cache.remove, ('{0}'.format(sample), ))
                 return True
 
-        bot.whisper(source.username, 'Your sample is not valid. Check out all the valid samples here: {0}/commands/playsound'.format(bot.domain))
+        bot.whisper(source.username, 'Your sample is not valid. Check out all the valid samples here: https://pajbot.com/playsounds')
         return False
 
     def load_commands(self, **options):
@@ -236,26 +238,5 @@ class PlaySoundTokenCommandModule(BaseModule):
                         'bot>user:Successfully played your sample fuckyou').parse(),
                     ],
                 )
-        global_script = """<script>
-            function playOrStopSound(elem, audio) {
-                if(elem.innerHTML=="Play") {
-                    elem.innerHTML="Stop";
-                    audio.play();
-                } else {
-                    elem.innerHTML="Play";
-                    audio.pause();
-                    audio.currentTime=0;
-                }
-            }
-            </script>"""
-        local_script = """<script>
-            var elem{0.command}=document.getElementById('btnTogglePlay{0.command}');
-            var snd{0.command} = new Audio("{0.href}");
-            snd{0.command}.onended=function(){{elem{0.command}.innerHTML='Play';}};
-            elem{0.command}.addEventListener("click", function(){{ playOrStopSound(elem{0.command}, snd{0.command}); }});
-        </script>"""
-        html_valid_samples = global_script
-        for sample in Samples.all_samples:
-            parsed_sample = local_script.format(sample)
-            html_valid_samples += ''.join(['<tr><td class="command-sample{1}">!#playsound {0.command}</td><td><button id="btnTogglePlay{0.command}">Play</button>{2}</td></tr>'.format(sample, ' new' if sample.new else '', parsed_sample)])
-        self.commands['#playsound'].long_description = '<h5 style="margin-top: 20px;">Valid samples</h5><table>{}</table>'.format(html_valid_samples)
+
+        self.commands['#playsound'].long_description = 'Playsounds can be tried out <a href="https://pajbot.com/playsounds">here</a>'
