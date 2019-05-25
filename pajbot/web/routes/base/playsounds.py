22ed2ebcad95aa3a9450e9266049c176a111b5d4
Ruben Anders <ruben.anders@robotty.de>
2019-05-25 12:17:17 +0200

    Fix internal server error if playsounds module was not initialized

--- pajbot/web/routes/base/playsounds.py
+++ pajbot/web/routes/base/playsounds.py
@@ -14,7 +14,10 @@ def init(app):
         with DBManager.create_session_scope() as session:
             playsounds = session.query(Playsound).filter(Playsound.enabled).all()
             playsound_module = session.query(Module).filter(Module.id == PlaysoundModule.ID).one()
-            settings = json.loads(playsound_module.settings)
+            if playsound_module.settings is None:
+                settings = dict(((setting.key, setting.default) for setting in PlaysoundModule.SETTINGS))
+            else:
+                settings = json.loads(playsound_module.settings)
 
             return render_template('playsounds.html', playsounds=playsounds, module_settings=settings,
                                    playsounds_enabled=playsound_module.enabled)
