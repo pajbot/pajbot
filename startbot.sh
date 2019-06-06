60b81a5d9068028c3246c33a965b3c1514f20070
Ruben Anders <ruben.anders@robotty.de>
2019-06-06 19:26:00 +0200

    Remove startbot script

--- startbot.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export LANG="en_US.utf8"
-
-LANG="en_US.utf8" PATH="`pwd`/myvenv/bin:$PATH" /usr/bin/pm2 start main.py --name="$1" --force --output="/var/log/pajbot/$1.log" --error="/var/log/pajbot/$1.err" --merge-logs -- --config configs/$1.ini
