a855c57eb2c850c66cd26cb3c4d70423fa08e1f0
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2019-07-13 16:22:11 +0200

    Update venvinstall script

--- install/venvinstall.sh
+++ install/venvinstall.sh
@@ -1,5 +1,21 @@
-#!/bin/bash
+#!/bin/sh
+
+set -e
+
+# Ensure this script is called from the correct folder
+if [ ! -d install ]; then
+    echo "This script needs to be called from the root folder, i.e. ./install/venvinstall.sh"
+    exit 1
+fi
+
+# Create virtual environment
 python3 -m venv myvenv
-source myvenv/bin/activate
+
+# Activate virtual environment
+. myvenv/bin/activate
+
+# Upgrade pip
 pip install pip --upgrade
+
+# Install requirements.txt
 pip install -r requirements.txt
