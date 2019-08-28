3f7a9d900a1f2cd2f5522735815c999040a920e0
Ruben Anders <ruben.anders@robotty.de>
2019-08-28 22:13:34 +0200

    Remove dead code in get user API endpoint

--- pajbot/web/routes/api/users.py
+++ pajbot/web/routes/api/users.py
@@ -12,14 +12,6 @@ class APIUser(Resource):
         if not user:
             return {"error": "Not found"}, 404
 
-        redis = RedisManager.get()
-        key = "{streamer}:users:num_lines".format(streamer=StreamHelper.get_streamer())
-        rank = redis.zrevrank(key, user.username)
-        if rank is None:
-            rank = redis.zcard(key)
-        else:
-            rank = rank + 1
-
         return user.jsonify()
 
 
