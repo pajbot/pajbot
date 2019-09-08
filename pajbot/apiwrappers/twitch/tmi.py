3f0d6fee2082d3f10aa38fcd696794bc72ed8f31
DatGuy1 <datguysteam@gmail.com>
2019-09-08 13:33:31 +0300

    Update tmi.py

--- pajbot/apiwrappers/twitch/tmi.py
+++ pajbot/apiwrappers/twitch/tmi.py
@@ -6,23 +6,16 @@ log = logging.getLogger(__name__)
 
 
 class TwitchTMIAPI(BaseTwitchAPI):
-    authorization_header_prefix = "OAuth"
-
     def __init__(self, client_credentials, redis):
         super().__init__(base_url="https://tmi.twitch.tv/", redis=redis)
         self.session.headers["Accept"] = "application/vnd.twitchtv.v5+json"
-        self.client_credentials = client_credentials
 
-    @property
-    def default_authorization(self):
-        return self.client_credentials
-
-    def get_chatters(self, streamer):
+    def fetch_chatters(self, streamer):
         response = self.get(["group", "user", streamer, "chatters"])
         ch = response["chatters"]
-
-        log.debug(ch)
-
-        chatters = ch["vips"] + ch["moderators"] + ch["staff"] + ch["admins"] + ch["global_mods"] + ch["viewers"]
+        chatters = []
+        
+        for chatter_category in chatters:
+            chatters.append(chatter_category)
 
         return chatters
