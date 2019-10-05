14213b079a96037259de008c2baef243d4a3e258
Ruben Anders <ruben.anders@robotty.de>
2019-10-05 16:45:17 +0200

    Fixed for new user model: Recent admin logs in admin home

--- pajbot/web/routes/admin/home.py
+++ pajbot/web/routes/admin/home.py
@@ -1,8 +1,8 @@
 from flask import render_template
+from sqlalchemy.orm import joinedload
 
-from pajbot.managers.adminlog import AdminLogManager
+from pajbot.managers.adminlog import AdminLogEntry
 from pajbot.managers.db import DBManager
-from pajbot.models.user import User
 from pajbot.web.utils import requires_level
 
 
@@ -10,16 +10,12 @@ def init(page):
     @page.route("/")
     @requires_level(500)
     def home(**options):
-        latest_logs_raw = AdminLogManager.get_entries()
-        cached_users = {}
         with DBManager.create_session_scope() as db_session:
-            latest_logs = []
-            for log in latest_logs_raw:
-                log["user"] = (
-                    cached_users[log["user_id"]]
-                    if log["user_id"] in cached_users
-                    else db_session.query(User).filter_by(id=log["user_id"]).one_or_none()
-                )
-                latest_logs.append(log)
-
+            latest_logs = (
+                db_session.query(AdminLogEntry)
+                .options(joinedload("user"))
+                .order_by(AdminLogEntry.created_at.desc())
+                .limit(50)
+                .all()
+            )
             return render_template("admin/home.html", latest_logs=latest_logs)
