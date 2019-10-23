03347394e76a4e23b86b0ffbcb7b17d2812f3093
Ruben Anders <ruben.anders@robotty.de>
2019-10-23 23:19:32 +0200

    Add BaseModule.is_enabled class method

--- pajbot/web/routes/base/playsounds.py
+++ pajbot/web/routes/base/playsounds.py
@@ -11,15 +11,10 @@ def init(app):
     def user_playsounds():
         with DBManager.create_session_scope() as session:
             playsounds = session.query(Playsound).filter(Playsound.enabled).order_by(Playsound.name).all()
-            playsound_module = session.query(Module).filter(Module.id == PlaysoundModule.ID).one_or_none()
-
-            enabled = False
-            if playsound_module is not None:
-                enabled = playsound_module.enabled
 
             return render_template(
                 "playsounds.html",
                 playsounds=playsounds,
                 module_settings=PlaysoundModule.module_settings(),
-                playsounds_enabled=enabled,
+                playsounds_enabled=PlaysoundModule.is_enabled(),
             )
