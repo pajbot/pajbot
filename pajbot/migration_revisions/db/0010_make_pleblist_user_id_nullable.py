f82e5b07b65a93316e543296cd11048f7bce4c2f
Ruben Anders <ruben.anders@robotty.de>
2019-10-23 16:29:37 +0200

    Add a long explanation to the top of the migration (#597)
    * Add a long explanation to the top of the migration
    
    * Change changelog wording
    
    * Python reformat

--- pajbot/migration_revisions/db/0010_make_pleblist_user_id_nullable.py
+++ pajbot/migration_revisions/db/0010_make_pleblist_user_id_nullable.py
@@ -1,9 +1,28 @@
-# This migration is here to correct a previous error in the 0004_unify_user_model migration, where the pleblist_song.user_id column was incorrectly marked as non-nullable
+# In release version 1.38, there was a bug with 0004_unify_user_model that would cause pleblist_song rows to be
+# deleted entirely if the referenced user (pleblist_song.user_id) was deleted. Instead, it was intended that on
+# those rows, user_id be nullable, and the user_id be set to NULL on delete of the referenced user.
+
+# Users that migrated their databases using version 1.38 are now in a database state where the foreign key constraints
+# and NOT NULL constraint need to be corrected.
+# For users that come from <=1.37 and upgrade using a bot of version >=1.39 will not encounter this problem,
+# because the issue with 0004_unify_user_model has been corrected in 1.39.
+
+# We could also have left 0004_unify_user_model untouched (keeping the bug), which would have eliminated the
+# need for the additional conditional migration below. We decided to modify 0004_unify_user_model instead,
+# so users running this migration in the future will not lose their pleblist_song rows and then will have to run
+# an extensive recovery routine to restore the lost rows. This way, only users who ran the bugged version of
+# 0004_unify_user_model will have to recover any deleted rows.
+
+# The conditional migration below first checks whether the database was migrated using the "bugged" or "fixed"
+# version of 0004_unify_user_model, and then executes the necessary schema changes to prepare the database for
+# a restore of the lost data.
+
+# Also see Pull Request #596: https://github.com/pajbot/pajbot/pull/596
 
 
 def up(cursor, bot):
-    # This query selects the "unnullable" state of the pleblist_song.user_id column
-    # We use this as a check for whether this schema has run migration 0004_unify_user_model in its non-fixed state, as that's what we're trying to remedy
+    # This query selects the "NOT NULL" state of the pleblist_song.user_id column, which we can use
+    # to determine whether this database was migrated with the bugged or fixed version of 0004_unify_user_model
     cursor.execute(
         """
     SELECT attnotnull
@@ -15,6 +34,7 @@ def up(cursor, bot):
     )
     pleblist_song_user_id_column_unnullable = cursor.fetchone()[0]
 
+    # If the column is marked NOT NULL, then the database was migrated with the bugged version of 0004_unify_user_model.
     if pleblist_song_user_id_column_unnullable:
         # Remove pleblist_song.user_id foreign key
         cursor.execute("ALTER TABLE pleblist_song DROP CONSTRAINT pleblist_song_user_id_fkey")
@@ -22,7 +42,7 @@ def up(cursor, bot):
         # Make pleblist_song.user_id column nullable
         cursor.execute("ALTER TABLE pleblist_song ALTER COLUMN user_id DROP NOT NULL")
 
-        # Re-add pleblist_song.user_id foreign key
+        # Re-add pleblist_song.user_id foreign key, with ON DELETE SET NULL (instead of ON DELETE CASCADE like before)
         cursor.execute(
             'ALTER TABLE pleblist_song ADD FOREIGN KEY (user_id) REFERENCES "user"(twitch_id) ON DELETE SET NULL'
         )
