7f0bbcd6616969b6bde0f6c83d1c1b1980b05225
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2019-10-23 15:42:13 +0200

    Add a new revision that only runs if the user has already run the "0004 Unify User Model" revision

---
+++ pajbot/migration_revisions/db/0010_make_pleblist_user_id_nullable.py
@@ -0,0 +1,27 @@
+# This migration is here to correct a previous error in the 0004_unify_user_model migration, where the pleblist_song.user_id column was incorrectly marked as non-nullable
+
+
+def up(cursor, bot):
+    # This query selects the "nullable" state of the pleblist_song.user_id column
+    cursor.execute(
+        """
+    SELECT attnotnull
+    FROM pg_attribute
+    WHERE
+        attrelid = 'pleblist_song'::regclass
+    and attname = 'user_id'
+    """
+    )
+    pleblist_song_user_id_column_unnullable = cursor.fetchone()[0]
+
+    if pleblist_song_user_id_column_unnullable:
+        # Remove pleblist_song.user_id foreign key
+        cursor.execute("ALTER TABLE pleblist_song DROP CONSTRAINT pleblist_song_user_id_fkey")
+
+        # Make pleblist_song.user_id column nullable
+        cursor.execute("ALTER TABLE pleblist_song ALTER COLUMN user_id DROP NOT NULL")
+
+        # Re-add pleblist_song.user_id foreign key
+        cursor.execute(
+            'ALTER TABLE pleblist_song ADD FOREIGN KEY (user_id) REFERENCES "user"(twitch_id) ON DELETE SET NULL'
+        )
