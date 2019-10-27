a847337d6c6deb650f048458745543c77d6deda5
Leppunen <niko@ivr.fi>
2019-10-27 23:19:43 +0200

    Add docker files

---
+++ scripts/run-docker.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+PBUID=$(id -u pajbot)
+PBGID=$(id -g pajbot)
+
+if [[ -z "${PBUID}${PBGID}" ]]; then
+  echo 'pajbot user not detected.'
+  exit 1
+fi
+
+if [ "$1" = "" ]; then
+    echo "No streamer name provided."
+    exit 1
+fi
+
+if [ ! -f /opt/configs/"$1".ini ]; then
+    echo "No config file /opt/configs/$1.ini found."
+    exit 1
+fi
+
+echo docker run \
+--name pajbot1-"$1" \
+--restart unless-stopped \
+-d \
+-e STREAMERNAME="$1" \
+-e TZ=UTC \
+--network host \
+-v /opt/configs/"$1".ini:/app/config.ini \
+-v /var/run/postgresql:/var/run/postgresql:ro \
+-v /etc/localtime:/etc/localtime:ro \
+-v /srv/pajbot:/srv/pajbot \
+-u "$PBUID":"$PBGID" \
+pajbot1:latest
