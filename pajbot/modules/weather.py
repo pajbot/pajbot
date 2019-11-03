63c5be740a440e9f7c832e4a52905520c1009f2f
ALazymeme <jkeasley@icloud.com>
2019-11-03 16:02:57 +1000

    Create weather.py

---
+++ pajbot/modules/weather.py
@@ -0,0 +1,10 @@
+from pajbot.modules.base import BaseModule
+from pajbot.modules.base import ModuleSetting
+
+class WeatherModule(BaseModule)
+
+    ID = 
+    NAME = "Weather"
+    DESCRIPTION = "Simple module for some weather commands"
+    CATEGORY = "Feature"
+    SETTINGS = []
