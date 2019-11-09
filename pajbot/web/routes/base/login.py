a83bd6ae9afd4004b570580535c81889a2e2fe0a
pajlada <rasmus.karlsson@pajlada.com>
2019-11-09 05:48:44 -0800

    fix `streamer_login` scope-checking to check for a proper subset instead of a subset (#662)
    * fix `streamer_login` scope-checking to check for a proper subset instead of a subset
    
    * Remove unneccesary f-string

--- pajbot/web/routes/base/login.py
+++ pajbot/web/routes/base/login.py
@@ -132,12 +132,12 @@ def init(app):
             # we only update their access token if the returned scope containes the special scopes requested
             # in /streamer_login
 
-            # We use issubset() to say "if the granted scope is a subset of the required scopes", this can be case
+            # We use < to say "if the granted scope is a proper subset of the required scopes", this can be case
             # for example when the bot is running in its own channel and you use /bot_login,
             # then the granted scopes will be a superset of the scopes needed for the streamer.
             # By doing this, both the streamer and bot token will be set if you complete /bot_login with the bot
             # account, and if the bot is running in its own channel.
-            if set(resp["scope"]).issubset(set(streamer_scopes)):
+            if set(resp["scope"]) < set(streamer_scopes):
                 log.info("Streamer logged in but not all scopes present, will not update streamer token")
             else:
                 redis = RedisManager.get()
