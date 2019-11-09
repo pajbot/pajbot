5184d783c3c691568810ca72d162d112b62aa233
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2019-11-09 13:22:02 +0100

    Update contact page with relevant information.
    There's now a link to my discord which has pajbot channels for support/questions.
    
    A section has been added to the config file that the /contact page
    reads, with the bot maintainer's information. currently the name and
    contact_string fields should be filled in. see kkonatestbroadcaster.ini
    for an example. This section lets you, the hoster who isn't pajlada, add
    contact information on the bot website.
    
    Fixes #652
    Fixed #659

--- pajbot/web/routes/base/contact.py
+++ pajbot/web/routes/base/contact.py
@@ -1,7 +1,22 @@
 from flask import render_template
 
+import logging
+
+log = logging.getLogger(__name__)
+
 
 def init(app):
+    try:
+        maintainer = {
+            "name": app.bot_config["maintainer"]["name"],
+            "contact_string": app.bot_config["maintainer"]["contact_string"],
+        }
+    except KeyError:
+        log.warn(
+            "Missing name and/or contact_string in config file, no direct contact info will be shown on the /contact page. See example config file"
+        )
+        maintainer = None
+
     @app.route("/contact")
     def contact():
-        return render_template("contact.html")
+        return render_template("contact.html", maintainer=maintainer)
