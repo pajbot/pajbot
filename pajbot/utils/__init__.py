3fbbf52603190055c6c744eb8923af1885e3a448
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2019-11-16 11:31:29 +0100

    Fixed Twitter statuses showing undecoded HTML entities
    Fixes #645

--- pajbot/utils/__init__.py
+++ pajbot/utils/__init__.py
@@ -14,9 +14,9 @@ from .parse_points_amount import parse_points_amount
 from .print_traceback import print_traceback
 from .remove_none_values import remove_none_values
 from .split_into_chunks_with_prefix import split_into_chunks_with_prefix
+from .stringify_tweet import stringify_tweet
 from .time_ago import time_ago
 from .time_limit import time_limit
 from .time_method import time_method
 from .time_since import time_since
-from .tweet_prettify_urls import tweet_prettify_urls
 from .wait_for_redis_data_loaded import wait_for_redis_data_loaded
--- pajbot/utils/tweet_prettify_urls.py
+++
@@ -1,6 +0,0 @@
-def tweet_prettify_urls(tweet):
-    tweet_text = tweet.text
-    for url in tweet.entities["urls"]:
-        tweet_text = tweet_text.replace(url["url"], url["expanded_url"])
-
-    return tweet_text
