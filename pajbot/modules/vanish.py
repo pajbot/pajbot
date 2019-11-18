829151b4c0b5328da14f29f6d68baad712dca281
ALazymeme <jkeasley@icloud.com>
2019-11-18 23:34:01 +1000

    Update vanish.py

--- pajbot/modules/vanish.py
+++ pajbot/modules/vanish.py
@@ -62,7 +62,7 @@ class VanishModule(BaseModule):
     ]
 
     @staticmethod
-    def vanish_command(bot, source, self, **rest):
+    def vanish_command(self, bot, source, **rest):
         bot.execute_delayed(0.5, bot.timeout, source, 1, reason=self.settings["timeout_reason"], once=True)
 
     def load_commands(self, **options):
