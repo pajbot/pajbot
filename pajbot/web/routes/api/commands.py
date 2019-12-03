d960a719a5361bcc30b0eff8acef7ee2c8d1874f
ALazymeme <jkeasley@icloud.com>
2019-12-03 20:41:30 +1000

    Update commands.py

--- pajbot/web/routes/api/commands.py
+++ pajbot/web/routes/api/commands.py
@@ -60,7 +60,7 @@ class APICommandRemove(Resource):
                 return {"error": "Invalid command ID"}, 404
             if command.level > options["user"].level:
                 return {"error": "Unauthorized"}, 403
-            log_msg = f"The !{command.command.split('|')[0]} command has been removed"
+            log_msg = f"The {self.prefix}{command.command.split('|')[0]} command has been removed"
             AdminLogManager.add_entry("Command removed", options["user"], log_msg)
             db_session.delete(command.data)
             db_session.delete(command)
@@ -158,9 +158,9 @@ class APICommandUpdate(Resource):
             command.data.set(**options)
 
             if len(old_message) > 0 and old_message != new_message:
-                log_msg = f'The !{command.command.split("|")[0]} command has been updated from "{old_message}" to "{new_message}"'
+                log_msg = f'The {self.prefix}{command.command.split("|")[0]} command has been updated from "{old_message}" to "{new_message}"'
             else:
-                log_msg = f"The !{command.command.split('|')[0]} command has been updated"
+                log_msg = f"The {self.prefix}{command.command.split('|')[0]} command has been updated"
 
             AdminLogManager.add_entry(
                 "Command edited",
