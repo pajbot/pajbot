2619145dbd5153bd70bb9062a1bce69d19d7ac95
TroyDota <49777269+TroyDota@users.noreply.github.com>
2019-12-16 16:27:44 +0200

    Update tmi.py

--- pajbot/tmi.py
+++ pajbot/tmi.py
@@ -1,8 +1,10 @@
 class TMI:
     message_limit = 90
-    whispers_message_limit = 20
-    whispers_limit_interval = 5  # in seconds
+    whispers_message_limit_second = 2
+    whispers_message_limit_minute = 90
 
     @staticmethod
     def promote_to_verified():
         TMI.message_limit = 7000
+        TMI.whispers_message_limit_second = 15
+        TMI.whispers_message_limit_minute = 1150
