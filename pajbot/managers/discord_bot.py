f088ec02c91695bf58f06b06dd095051bf60b7af
TroyDota <troybensonsa@gmail.com>
2020-01-25 14:30:11 +0200

    Update discord_bot.py

--- pajbot/managers/discord_bot.py
+++ pajbot/managers/discord_bot.py
@@ -157,6 +157,7 @@ class DiscordBotManager(object):
                     all_users_con = UserConnections._by_tier(db_session, requested_tier)
                     if requested_tier == 0:
                         all_users_con = all_users_con + UserConnections._by_tier(db_session, None)
+                    log.info(len(all_users_con))
                     for user_con in all_users_con:
                         user = user_con.twitch_user
                         if (user.tier is None and requested_tier != 0) or user.tier != requested_tier:
