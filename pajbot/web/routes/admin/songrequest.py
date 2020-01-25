5643c20b40d33219a4769b09c1cf972bb024c8cb
TroyDota <troybensonsa@gmail.com>
2020-01-25 08:57:58 +0200

    Update songrequest.py

--- pajbot/web/routes/admin/songrequest.py
+++ pajbot/web/routes/admin/songrequest.py
@@ -12,7 +12,7 @@ log = logging.getLogger(__name__)
 def init(page):
     @page.route("/admin/songrequest")
     @requires_level(500, "/admin/songrequest")
-    def songrequest(**options):
+    def admin_songrequest(**options):
         if session.get("twitch_token_expire", 0) <= round(time.time()):
             return redirect("/login?n=/admin/songrequest")
         return render_template("admin/songrequest.html", token_access=session.get("twitch_token"))
