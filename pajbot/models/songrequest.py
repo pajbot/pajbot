10d0d37f6ad7047196f8399f8b10df7e0dbcf409
TroyDota <troybensonsa@gmail.com>
2020-01-25 06:55:00 +0200

    fixed

--- pajbot/models/songrequest.py
+++ pajbot/models/songrequest.py
@@ -311,7 +311,7 @@ class SongrequestHistory(Base):
     def _get_history(db_session, limit):
         played_songs = (
             db_session.query(SongrequestHistory)
-            .filter(SongrequestHistory.song_info.has(banned = False))
+            .filter(SongrequestHistory.song_info.has(banned=False))
             .order_by(SongrequestHistory.id.desc())
             .limit(limit)
             .all()
--- pajbot/web/routes/base/songrequest.py
+++ pajbot/web/routes/base/songrequest.py
@@ -29,7 +29,7 @@ def init(app):
             songs_queue = []
             queue = (
                 db_session.query(SongrequestQueue)
-                .filter(SongrequestHistory.song_info.has(banned = False))
+                .filter(SongrequestHistory.song_info.has(banned=False))
                 .order_by(SongrequestQueue.queue)
                 .limit(50)
                 .all()
@@ -53,7 +53,7 @@ def init(app):
 
             history = (
                 db_session.query(SongrequestHistory)
-                .filter(SongRequestSongInfo.banned == False)
+                .filter(SongrequestHistory.song_info.has(banned=False))
                 .order_by(SongrequestHistory.id.desc())
                 .limit(50)
                 .all()
