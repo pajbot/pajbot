51d9b301f948ffa4ac6342e1c2f2c4e780dd628e
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2020-02-02 15:47:59 +0100

    add a redis migration to fix some emote IDs being ints

---
+++ pajbot/migration_revisions/redis/0002_fix_bad_quest_emote_json.py
@@ -0,0 +1,18 @@
+import json
+
+
+def up(redis, bot):
+    # invalidate bad emotes cache
+    redis.delete("api:twitch:kraken:v5:global-emotes")
+    redis.delete(f"api:twitch_emotes:channel-emotes:{bot.streamer}")
+    redis.delete(f"api:ffz:channel-emotes:{bot.streamer}")
+    redis.delete("api:ffz:global-emotes")
+
+    current_emote_key = f"{bot.streamer}:current_quest_emote"
+    current_emote_json = redis.get(current_emote_key)
+    if current_emote_json is not None:
+        # ensure emote id is a string
+        current_emote = json.loads(current_emote_json)
+        if not isinstance(current_emote["id"], str):
+            current_emote["id"] = str(current_emote["id"])
+            redis.set(current_emote_key, json.dumps(current_emote))
