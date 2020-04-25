a95a83415c2612d48fa95e7f410865976a147e58
ALazymeme <jkeasley@icloud.com>
2020-04-25 22:23:44 +1000

    Update followalert.py

--- pajbot/modules/chat_alerts/followalert.py
+++ pajbot/modules/chat_alerts/followalert.py
@@ -73,18 +73,17 @@ class FollowAlertModule(BaseModule):
     def __init__(self, bot):
         super().__init__(bot)
 
-    def on_sub_shared(self, user):
+    def on_follow(self, user):
         if self.settings["grant_points_on_follow"] <= 0:
             return
 
         user.points += self.settings["grant_points_on_follow"]
         self.bot.say(f"{user} was given {self.settings['grant_points_on_follow']} points for following! FeelsAmazingMan")
 
-    def on_new_sub(self, user, sub_type, gifted_by=None):
+    def on_follow(self, user):
         """
-        A new user just subscribed.
+        A new user just followed.
         Send the event to the websocket manager, and send a customized message in chat.
-        Also increase the number of active subscribers in the database by one.
         """
 
         self.on_sub_shared(user)
@@ -107,111 +106,3 @@ class FollowAlertModule(BaseModule):
             self.bot.execute_delayed(
                 self.settings["whisper_after"], self.bot.whisper, user, self.get_phrase("new_sub_whisper", **payload)
             )
-
-    def on_resub(self, user, num_months, sub_type, gifted_by=None, substreak_count=0):
-        """
-        A user just re-subscribed.
-        Send the event to the websocket manager, and send a customized message in chat.
-        """
-
-        self.on_sub_shared(user)
-
-        payload = {"username": user.name, "num_months": num_months, "gifted_by": gifted_by}
-        if substreak_count and substreak_count > 0:
-            payload["substreak_string"] = self.get_phrase(
-                "substreak_string", username=user.name, num_months=substreak_count, gifted_by=gifted_by
-            )
-        else:
-            payload["substreak_string"] = ""
-        self.bot.websocket_manager.emit("resub", payload)
-
-        if self.settings["chat_message"] is True:
-            if sub_type == "Prime":
-                self.bot.say(self.get_phrase("resub_prime", **payload))
-            else:
-                if gifted_by:
-                    self.bot.say(self.get_phrase("resub_gift", **payload))
-                else:
-                    self.bot.say(self.get_phrase("resub", **payload))
-
-        if self.settings["whisper_message"] is True:
-            self.bot.execute_delayed(
-                self.settings["whisper_after"], self.bot.whisper, user, self.get_phrase("resub_whisper", **payload)
-            )
-
-    def on_usernotice(self, source, tags, **rest):
-        if "msg-id" not in tags:
-            return
-
-        if tags["msg-id"] == "resub":
-            num_months = -1
-            substreak_count = 0
-            if "msg-param-months" in tags:
-                num_months = int(tags["msg-param-months"])
-            if "msg-param-cumulative-months" in tags:
-                num_months = int(tags["msg-param-cumulative-months"])
-            if "msg-param-streak-months" in tags:
-                substreak_count = int(tags["msg-param-streak-months"])
-            if "msg-param-should-share-streak" in tags:
-                should_share = bool(tags["msg-param-should-share-streak"])
-                if not should_share:
-                    substreak_count = 0
-
-            if "msg-param-sub-plan" not in tags:
-                log.debug(f"subalert msg-id is resub, but missing msg-param-sub-plan: {tags}")
-                return
-
-            # log.debug('msg-id resub tags: {}'.format(tags))
-
-            # TODO: Should we check room id with streamer ID here? Maybe that's for pajbot2 instead
-            self.on_resub(source, num_months, tags["msg-param-sub-plan"], None, substreak_count)
-            HandlerManager.trigger("on_user_resub", user=source, num_months=num_months)
-        elif tags["msg-id"] == "subgift":
-            num_months = 0
-            substreak_count = 0
-            if "msg-param-months" in tags:
-                num_months = int(tags["msg-param-months"])
-            if "msg-param-cumulative-months" in tags:
-                num_months = int(tags["msg-param-cumulative-months"])
-            if "msg-param-streak-months" in tags:
-                substreak_count = int(tags["msg-param-streak-months"])
-            if "msg-param-should-share-streak" in tags:
-                should_share = bool(tags["msg-param-should-share-streak"])
-                if not should_share:
-                    substreak_count = 0
-
-            if "display-name" not in tags:
-                log.debug(f"subalert msg-id is subgift, but missing display-name: {tags}")
-                return
-
-            with DBManager.create_session_scope() as db_session:
-                receiver_id = tags["msg-param-recipient-id"]
-                receiver_login = tags["msg-param-recipient-user-name"]
-                receiver_name = tags["msg-param-recipient-display-name"]
-                receiver = User.from_basics(db_session, UserBasics(receiver_id, receiver_login, receiver_name))
-
-                if num_months > 1:
-                    # Resub
-                    self.on_resub(
-                        receiver, num_months, tags["msg-param-sub-plan"], tags["display-name"], substreak_count
-                    )
-                    HandlerManager.trigger("on_user_resub", user=receiver, num_months=num_months)
-                else:
-                    # New sub
-                    self.on_new_sub(receiver, tags["msg-param-sub-plan"], tags["display-name"])
-                    HandlerManager.trigger("on_user_sub", user=receiver)
-        elif tags["msg-id"] == "sub":
-            if "msg-param-sub-plan" not in tags:
-                log.debug(f"subalert msg-id is sub, but missing msg-param-sub-plan: {tags}")
-                return
-
-            self.on_new_sub(source, tags["msg-param-sub-plan"])
-            HandlerManager.trigger("on_user_sub", user=source)
-        else:
-            log.debug(f"Unhandled msg-id: {tags['msg-id']} - tags: {tags}")
-
-    def enable(self, bot):
-        HandlerManager.add_handler("on_usernotice", self.on_usernotice)
-
-    def disable(self, bot):
-        HandlerManager.remove_handler("on_usernotice", self.on_usernotice)
