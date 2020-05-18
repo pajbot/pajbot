d355dc123dd17d22b307fae0e1f08105e5e3b354
troydota <troybensonsa@gmail.com>
2020-05-18 07:55:04 +0200

    Update stream_update.py

--- pajbot/modules/basic/stream_update.py
+++ pajbot/modules/basic/stream_update.py
@@ -61,7 +61,7 @@ class StreamUpdateModule(BaseModule):
             except HTTPError as e:
                 if e.response.status_code == 401:
                     bot.say(
-                        f"Error (bot operator): The streamer needs to be re-authenticated to be able to update the {field}."
+                        f"Error: Niether the streamer nor the bot token grants permission to update the {field}. The streamer needs to be re-authenticated to fix this problem."
                     )
                     return
                 else:
