19aa59cf5111e385e03a396dd27990c9b2714428
TroyDota <49777269+TroyDota@users.noreply.github.com>
2020-05-24 14:27:00 +0200

    Update 0012_badges.py

--- pajbot/migration_revisions/db/0012_badges.py
+++ pajbot/migration_revisions/db/0012_badges.py
@@ -1,3 +1,3 @@
 def up(cursor, bot):
-    cursor.execute('ALTER TABLE "user" ADD COLUMN IF NOT EXISTS founder BOOLEAN NOT NULL DEFAULT FALSE')
-    cursor.execute('ALTER TABLE "user" ADD COLUMN IF NOT EXISTS vip BOOLEAN NOT NULL DEFAULT FALSE')
+    cursor.execute('ALTER TABLE "user" ADD COLUMN founder BOOLEAN NOT NULL DEFAULT FALSE')
+    cursor.execute('ALTER TABLE "user" ADD COLUMN vip BOOLEAN NOT NULL DEFAULT FALSE')
