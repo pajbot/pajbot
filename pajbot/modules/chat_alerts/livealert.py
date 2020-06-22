689f0d4c33e96327f869890977c2b18547911860
ALazyMeme <jkeasley@icloud.com>
2020-06-22 15:31:26 +0000

    Added live alert module

--- pajbot/modules/chat_alerts/livealert.py
+++ pajbot/modules/chat_alerts/livealert.py
@@ -4,9 +4,9 @@ from pajbot.managers.handler import HandlerManager
 from pajbot.modules import BaseModule
 from pajbot.modules import ModuleSetting
 from pajbot.modules.chat_alerts import ChatAlertModule
-from pajbot.models.stream import StreamManager
 
 log = logging.getLogger(__name__)
+
 
 class LiveAlertModule(BaseModule):
     ID = __name__.split(".")[-1]
@@ -24,15 +24,21 @@ class LiveAlertModule(BaseModule):
             placeholder="{streamer} is now live! PogChamp Streaming {game}: {title}",
             default="{streamer} is now live! PogChamp Streaming {game}: {title}",
             constraints={"max_str_len": 400},
-        ),
+        )
     ]
 
     def __init__(self, bot):
         super().__init__(bot)
 
-    def on_stream_start (self, bot, stream, **rest):
+    def on_stream_start(self, **rest):
         live_chat_message = self.settings["live_message"]
-        bot.say(live_chat_message.format(streamer=bot.self.streamer, game=stream.self.game, title=stream.self.title))
+        self.bot.say(
+            live_chat_message.format(
+                streamer=self.bot.streamer_display,
+                game=self.bot.stream_manager.game,
+                title=self.bot.stream_manager.title,
+            )
+        )
 
     def enable(self, bot):
         HandlerManager.add_handler("on_stream_start", self.on_stream_start)
