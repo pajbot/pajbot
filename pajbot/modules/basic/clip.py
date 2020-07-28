86d38cf21709950d20725f263c5605687ce450f4
ALazyMeme <jkeasley@icloud.com>
2020-07-28 01:27:41 +0000

    Fixed auth

--- pajbot/modules/basic/clip.py
+++ pajbot/modules/basic/clip.py
@@ -108,10 +108,10 @@ class ClipCommandModule(BaseModule):
                 )
             return True
 
-        clip_id = self.bot.twitch_helix_api.create_clip(
-            self.bot.streamer_user_id, self.bot.twitch_helix_api.default_authorization
+        clip_id = self.bot.twitch_helix_api.create_clip(self.bot.streamer_user_id, self.bot.bot_token_manager)
+        clip_url = self.bot.twitch_helix_api.get_clips(
+            StreamHelper.get_streamer_id(), clip_id, self.bot.bot_token_manager
         )
-        clip_url = self.bot.twitch_helix_api.get_clips()
         self.bot.say(
             self.settings["online_response"].format(source=source, streamer=self.bot.streamer_display, clip=clip_url)
         )
