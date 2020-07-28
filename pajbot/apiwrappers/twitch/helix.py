176fed19a47cca33d43919a9e449487de3cca7fe
ALazyMeme <jkeasley@icloud.com>
2020-07-28 11:34:01 +1000

    Remove broadcaster_id from clip fetching

--- pajbot/apiwrappers/twitch/helix.py
+++ pajbot/apiwrappers/twitch/helix.py
@@ -254,7 +254,7 @@ class TwitchHelixAPI(BaseTwitchAPI):
 
         return clip_id
 
-    def get_clips(self, broadcaster_id, clip_id, authorization):
+    def get_clips(self, clip_id, authorization):
         response = self.get("/clips", {"id": clip_id}, authorization=authorization)
         clip_url = [entry["url"] for entry in response["data"]]
 
