0312c315c57d9cea7c58188c4bffea4789281f1f
ALazyMeme <jkeasley@icloud.com>
2020-08-01 23:18:53 +1000

    Revert "Add broadcaster to the DB"
    This reverts commit e0fa3f584d44a8604ba70fe0e2ae1b965e16faac.

--- pajbot/migration_revisions/db/0012_badges.py
+++ pajbot/migration_revisions/db/0012_badges.py
@@ -1,4 +1,3 @@
 def up(cursor, bot):
     cursor.execute('ALTER TABLE "user" ADD COLUMN founder BOOLEAN NOT NULL DEFAULT FALSE')
     cursor.execute('ALTER TABLE "user" ADD COLUMN vip BOOLEAN NOT NULL DEFAULT FALSE')
-    cursor.execute('ALTER TABLE "user" ADD COLUMN broadcaster BOOLEAN NOT NULL DEFAULT FALSE')
