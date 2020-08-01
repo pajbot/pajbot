e0fa3f584d44a8604ba70fe0e2ae1b965e16faac
ALazyMeme <jkeasley@icloud.com>
2020-08-01 18:02:21 +1000

    Add broadcaster to the DB

--- pajbot/migration_revisions/db/0012_badges.py
+++ pajbot/migration_revisions/db/0012_badges.py
@@ -1,3 +1,4 @@
 def up(cursor, bot):
     cursor.execute('ALTER TABLE "user" ADD COLUMN founder BOOLEAN NOT NULL DEFAULT FALSE')
     cursor.execute('ALTER TABLE "user" ADD COLUMN vip BOOLEAN NOT NULL DEFAULT FALSE')
+    cursor.execute('ALTER TABLE "user" ADD COLUMN broadcaster BOOLEAN NOT NULL DEFAULT FALSE')
