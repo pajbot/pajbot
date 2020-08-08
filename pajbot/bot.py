44dccf3cb1dc39c0596595fd7e0bd256eec30dc7
TroyDota <49777269+TroyDota@users.noreply.github.com>
2020-08-08 15:06:53 +0200

    Store Founder & Badge status for users (#886)
    This allows for the modules to use the fact that they are "founders" or that they are "vips" allowing for, when i add my songrequest module vip/founder only requests

--- pajbot/bot.py
+++ pajbot/bot.py
@@ -661,6 +661,10 @@ class Bot:
             source.moderator = tags["mod"] == "1" or source.id == self.streamer_user_id
             # Having the founder badge means that the subscriber tag is set to 0. Therefore it's more stable to just check badges
             source.subscriber = "founder" in badges or "subscriber" in badges
+            # once they are a founder they are always be a founder, regardless if they are a sub or not.
+            if not source.founder:
+                source.founder = "founder" in badges
+            source.vip = "vip" in badges
 
         if not whisper and source.banned:
             self.ban(
