9d644552d9d4893881052f984b91c09583ce9939
Ruben Anders <ruben.anders@robotty.de>
2020-10-04 10:59:55 +0200

    fix the logic, oops

--- pajbot/models/command.py
+++ pajbot/models/command.py
@@ -372,13 +372,13 @@ class Command(Base):
             # User is not a sub or a moderator, and cannot use the command.
             return False
 
-        # self.level < Command.BYPASS_MOD_ONLY_LEVEL because otherwise the mod_only flag would have no purpose
+        # self.level >= Command.BYPASS_MOD_ONLY_LEVEL because otherwise the mod_only flag would have no purpose
         # when set on commands with level >= BYPASS_MOD_ONLY_LEVEL. (The mod_only requirement could always
         # be bypassed)
         if (
             self.mod_only
             and source.moderator is False
-            and self.level < Command.BYPASS_MOD_ONLY_LEVEL
+            and self.level >= Command.BYPASS_MOD_ONLY_LEVEL
             and source.level < Command.BYPASS_MOD_ONLY_LEVEL
         ):
             # User is not a twitch moderator, or a bot moderator
