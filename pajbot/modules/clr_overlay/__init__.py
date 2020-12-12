ed74866691942afa6b115d7557b14908549b6a6d
ALazyMeme <jkeasley@icloud.com>
2020-12-12 21:51:03 +1000

    Fix wording

--- pajbot/modules/clr_overlay/__init__.py
+++ pajbot/modules/clr_overlay/__init__.py
@@ -9,7 +9,7 @@ log = logging.getLogger(__name__)
 class CLROverlayModule(BaseModule):
     ID = "clroverlay-group"
     NAME = "CLR Overlay"
-    DESCRIPTION = "A collection overlays that can be used in the streaming software of choice"
+    DESCRIPTION = "A collection of overlays that can be used in the streaming software of choice"
     CATEGORY = "Feature"
     ENABLED_DEFAULT = True
     MODULE_TYPE = ModuleType.TYPE_ALWAYS_ENABLED
