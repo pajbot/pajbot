3954bac5785afbae194f1d8ff55aff73f4c3dd04
ALazyMeme <jkeasley@icloud.com>
2021-01-30 22:25:37 +1000

    Add online/offline option for the ASCII module

--- pajbot/modules/ascii.py
+++ pajbot/modules/ascii.py
@@ -17,6 +17,14 @@ class AsciiProtectionModule(BaseModule):
     CATEGORY = "Moderation"
     SETTINGS = [
         ModuleSetting(
+            key="stream_status",
+            label="Allow ASCII characters while the stream is:",
+            type="options",
+            required=True,
+            default="Neither offline nor online",
+            options=["Online", "Offline", "Neither offline nor online"],
+        ),
+        ModuleSetting(
             key="min_msg_length",
             label="Minimum message length to be considered bad",
             type="number",
@@ -82,6 +90,12 @@ class AsciiProtectionModule(BaseModule):
         return False
 
     def on_pubmsg(self, source, message, **rest):
+        if self.settings["stream_status"] == "Online" and self.bot.is_online:
+            return
+
+        if self.settings["stream_status"] == "Offline" and not self.bot.is_online:
+            return
+
         if source.level >= self.settings["bypass_level"] or source.moderator is True:
             return
 
