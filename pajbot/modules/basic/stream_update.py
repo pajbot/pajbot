c041684aca53357cb8129b9e72ebb8d4caf3fcb1
ALazyMeme <jkeasley@icloud.com>
2021-08-25 23:54:02 +1000

    FIX

--- pajbot/modules/basic/stream_update.py
+++ pajbot/modules/basic/stream_update.py
@@ -87,7 +87,7 @@ class StreamUpdateModule(BaseModule):
             self.bot.twitch_helix_api.modify_channel_information(
                 self.bot.streamer_user_id,
                 authorization=bot.streamer_access_token_manager,
-                params={"game_id": game.id},
+                json={"game_id":game.id},
             )
         except HTTPError as e:
             if e.response.status_code == 500:
@@ -113,7 +113,7 @@ class StreamUpdateModule(BaseModule):
             self.bot.twitch_helix_api.modify_channel_information(
                 self.bot.streamer_user_id,
                 authorization=bot.streamer_access_token_manager,
-                params={"title": message},
+                json={"title":message},
             )
         except HTTPError as e:
             if e.response.status_code == 400:
