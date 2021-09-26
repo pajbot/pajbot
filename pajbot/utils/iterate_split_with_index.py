0e53f34765c51846af7d7b4a6c130d14e69e4501
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2021-09-26 12:56:53 +0200

    Add typing to utils/iterate_split_with_index.py

--- pajbot/utils/iterate_split_with_index.py
+++ pajbot/utils/iterate_split_with_index.py
@@ -1,4 +1,9 @@
-def iterate_split_with_index(split_parts, separator_length=1):
+from typing import Generator, Tuple, List
+
+
+def iterate_split_with_index(
+    split_parts: List[str], separator_length: int = 1
+) -> Generator[Tuple[int, str], None, None]:
     """Generator function that for a given list of split parts of a string,
     returns a tuple with the starting index of that split word/part (in the original string) and the part"""
     index = 0
