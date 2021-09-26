c33d8a42810ce39e8619a08fc040e5f6d18cce96
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2021-09-26 12:59:07 +0200

    add typing to utils/now.py

--- pajbot/utils/now.py
+++ pajbot/utils/now.py
@@ -1,7 +1,7 @@
 import datetime
 
 
-def now():
+def now() -> datetime.datetime:
     """
     Returns a timezone-aware datetime object representing the current universal coordinated time (UTC).
     E.g.: datetime.datetime(2019, 5, 31, 14, 36, 49, 861063, tzinfo=datetime.timezone.utc)
