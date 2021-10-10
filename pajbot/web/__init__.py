29f14b0d0d287ea131384591ef02a0883d324f09
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2021-10-10 11:34:43 +0200

    make use of get_boolean helper in web init

--- pajbot/web/__init__.py
+++ pajbot/web/__init__.py
@@ -150,7 +150,7 @@ def init(args):
         "bot": {"name": config["main"]["nickname"]},
         "site": {
             "domain": config["web"]["domain"],
-            "deck_tab_images": config["web"].get("deck_tab_images", "0") == "1",
+            "deck_tab_images": cfg.get_boolean(config["web"], "deck_tab_images", False),
             "websocket": {"host": config["websocket"].get("host", f"wss://{config['web']['domain']}/clrsocket")},
         },
         "streamer": {"name": streamer_display, "full_name": config["main"]["streamer"], "id": streamer_user_id},
