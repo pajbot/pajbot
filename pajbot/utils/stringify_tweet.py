f5c2e373f469b0ab1b4c923e618ef4c083c00fd4
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2021-10-10 11:43:57 +0200

    Expect a string-keyed dict in tweet-stringifying functions

--- pajbot/utils/stringify_tweet.py
+++ pajbot/utils/stringify_tweet.py
@@ -23,7 +23,7 @@ def stringify_tweet(tweet: Tweet) -> str:
     return html.unescape(tweet_text).replace("\n", " ")
 
 
-def _tweet_provider_tweet_extract_text_with_expanded_urls(tweet: Dict[Any, Any]) -> str:
+def _tweet_provider_tweet_extract_text_with_expanded_urls(tweet: Dict[str, Any]) -> str:
     tweet_text: str = tweet["text"]
     for url in tweet["urls"]:
         tweet_text = tweet_text.replace(url["url"], url["expanded_url"])
@@ -32,7 +32,7 @@ def _tweet_provider_tweet_extract_text_with_expanded_urls(tweet: Dict[Any, Any])
 
 
 # Extract the text from a tweet and make it prettier
-def tweet_provider_stringify_tweet(tweet: Dict[Any, Any]) -> str:
+def tweet_provider_stringify_tweet(tweet: Dict[str, Any]) -> str:
     tweet_text = _tweet_provider_tweet_extract_text_with_expanded_urls(tweet)
 
     return html.unescape(tweet_text).replace("\n", " ")
