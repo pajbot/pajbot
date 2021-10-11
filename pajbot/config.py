b45a520f9e592a08962dc26aca964fcbb8e16bf7
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2021-10-11 21:41:32 +0200

    config.py: get_boolean simplify method due to change in ConfigSection type

--- pajbot/config.py
+++ pajbot/config.py
@@ -24,16 +24,7 @@ def load_twitter_manager(config: Config) -> Type[GenericTwitterManager]:
     return TwitterManager
 
 
-def get_boolean(o: Dict[str, Any], key: str, default_value: bool) -> bool:
+def get_boolean(o: ConfigSection, key: str, default_value: bool) -> bool:
     v = o.get(key, default_value)
 
-    if isinstance(v, str):
-        return v == "1"
-
-    if isinstance(v, int):
-        return v == 1
-
-    if isinstance(v, bool):
-        return v
-
-    raise Exception(f"Invalid type read for get_boolean, got {type(v)}, expected str, int, or bool")
+    return v == "1"
