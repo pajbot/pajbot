9b2ec74baa4771365c51712fcf250ad045f656cf
pajlada <rasmus.karlsson@pajlada.com>
2021-10-17 12:32:19 +0200

    Add typing to apiwrappers/base.py (#1551)
    * apiwrappers/base.py: sort imports
    
    * apiwrappers/base.py: BaseAPI.__init__ return and parameter typing
    
    * apiwrappers/base.py: BaseAPI.quote_path_param return and parameter typing
    
    * apiwrappers/base.py: BaseAPI.fill_in_url_scheme return and parameter typing
    
    * apiwrappers/base.py: BaseAPI.parse_datetime return and parameter typing
    
    * apiwrappers/base.py: BaseAPI.join_base_and_string return and parameter typing
    
    * apiwrappers/base.py: BaseAPI.join_base_and_list return and parameter typing
    
    * apiwrappers/base.py: BaseAPI.join_base_and_endpoint return and parameter typing
    
    * apiwrappers/base.py: BaseAPI.request return and parameter typing
    
    * apiwrappers/base.py: BaseAPI.get return and parameter typing
    
    * apiwrappers/base.py: BaseAPI.get_response return and parameter typing
    
    * apiwrappers/base.py: BaseAPI.get_binary return and parameter typing
    
    * apiwrappers/base.py: BaseAPI.post return and parameter typing
    
    * apiwrappers/base.py: BaseAPI.put return and parameter typing
    
    * apiwrappers/base.py: BaseAPI.patch return and parameter typing

--- pajbot/apiwrappers/base.py
+++ pajbot/apiwrappers/base.py
@@ -1,18 +1,26 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any, List, Optional, Union
+
+import datetime
 import logging
 from urllib.parse import quote, urlparse, urlunparse
 
-import datetime
-
-from requests import Session
-
 from pajbot import constants
 from pajbot.apiwrappers.response_cache import APIResponseCache
+
+from requests import Session, Response
+
+if TYPE_CHECKING:
+    from pajbot.managers.redis import RedisType
+
+AnyEndpoint = Union[List[Any], str]
 
 log = logging.getLogger(__name__)
 
 
 class BaseAPI:
-    def __init__(self, base_url, redis=None):
+    def __init__(self, base_url: str, redis: Optional[RedisType] = None) -> None:
         self.base_url = base_url
 
         self.session = Session()
@@ -25,11 +33,11 @@ class BaseAPI:
             self.cache = APIResponseCache(redis)
 
     @staticmethod
-    def quote_path_param(param):
+    def quote_path_param(param: str) -> str:
         return quote(param, safe="")
 
     @staticmethod
-    def fill_in_url_scheme(url, default_scheme="https"):
+    def fill_in_url_scheme(url: str, default_scheme: str = "https") -> str:
         """Fill in the scheme part of a given URL string, e.g.
         with given inputs of url = "//example.com/abc" and
         default_scheme="https", the output would be
@@ -41,14 +49,14 @@ class BaseAPI:
         return urlunparse(parsed_template)
 
     @staticmethod
-    def parse_datetime(datetime_str):
+    def parse_datetime(datetime_str: str) -> datetime.datetime:
         """Parses date strings in the format of 2015-09-11T23:01:11Z
         to a tz-aware datetime object."""
         naive_dt = datetime.datetime.strptime(datetime_str, "%Y-%m-%dT%H:%M:%SZ")
         return naive_dt.replace(tzinfo=datetime.timezone.utc)
 
     @staticmethod
-    def join_base_and_list(base, path_segments):
+    def join_base_and_list(base: str, path_segments: List[Any]) -> str:
         url = base
         for path_segment in path_segments:
             # str(endpoint) so numbers can be used as path segments too
@@ -57,23 +65,31 @@ class BaseAPI:
         return url
 
     @staticmethod
-    def join_base_and_string(base, endpoint):
+    def join_base_and_string(base: str, endpoint: str) -> str:
         base = base.rstrip("/")
         endpoint = endpoint.lstrip("/")
         return base + "/" + endpoint
 
     @staticmethod
-    def join_base_and_endpoint(base, endpoint):
+    def join_base_and_endpoint(base: Optional[str], endpoint: AnyEndpoint) -> str:
         # For use cases with no base and absolute endpoint URLs
         if base is None:
-            return endpoint
+            return str(endpoint)
 
         if isinstance(endpoint, list):
             return BaseAPI.join_base_and_list(base, endpoint)
         else:
             return BaseAPI.join_base_and_string(base, endpoint)
 
-    def request(self, method, endpoint, params, headers, json=None, **request_options):
+    def request(
+        self,
+        method: str,
+        endpoint: AnyEndpoint,
+        params: Any,
+        headers: Any,
+        json: Optional[Any] = None,
+        **request_options: Any,
+    ) -> Response:
         full_url = self.join_base_and_endpoint(self.base_url, endpoint)
         response = self.session.request(
             method, full_url, params=params, headers=headers, json=json, timeout=self.timeout, **request_options
@@ -81,20 +97,30 @@ class BaseAPI:
         response.raise_for_status()
         return response
 
-    def get(self, endpoint, params=None, headers=None, **request_options):
+    def get(self, endpoint: AnyEndpoint, params: Any = None, headers: Any = None, **request_options: Any) -> Any:
         return self.request("GET", endpoint, params, headers, **request_options).json()
 
-    def get_response(self, endpoint, params=None, headers=None, **request_options):
+    def get_response(
+        self, endpoint: AnyEndpoint, params: Any = None, headers: Any = None, **request_options: Any
+    ) -> Response:
         return self.request("GET", endpoint, params, headers, **request_options)
 
-    def get_binary(self, endpoint, params=None, headers=None, **request_options):
+    def get_binary(
+        self, endpoint: AnyEndpoint, params: Any = None, headers: Any = None, **request_options: Any
+    ) -> bytes:
         return self.request("GET", endpoint, params, headers, **request_options).content
 
-    def post(self, endpoint, params=None, headers=None, json=None, **request_options):
+    def post(
+        self, endpoint: AnyEndpoint, params: Any = None, headers: Any = None, json: Any = None, **request_options: Any
+    ) -> Any:
         return self.request("POST", endpoint, params, headers, json, **request_options).json()
 
-    def put(self, endpoint, params=None, headers=None, json=None, **request_options):
+    def put(
+        self, endpoint: AnyEndpoint, params: Any = None, headers: Any = None, json: Any = None, **request_options: Any
+    ) -> Any:
         return self.request("PUT", endpoint, params, headers, json, **request_options).json()
 
-    def patch(self, endpoint, params=None, headers=None, json=None, **request_options):
+    def patch(
+        self, endpoint: AnyEndpoint, params: Any = None, headers: Any = None, json: Any = None, **request_options: Any
+    ) -> Response:
         return self.request("PATCH", endpoint, params, headers, json, **request_options)
