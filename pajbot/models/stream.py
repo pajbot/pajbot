d17317d1ed9eabb5c735fff12ba55009e67af59b
pajlada <rasmus.karlsson@pajlada.com>
2021-10-17 13:40:02 +0200

    Add typing to models/stream.py (#1516)
    * models/stream.py: Stream.__init__ return and parameter typing
    
    * models/stream.py: sort imports
    
    * models/stream.py: add annotations

--- pajbot/models/stream.py
+++ pajbot/models/stream.py
@@ -1,23 +1,21 @@
-import logging
+from __future__ import annotations
 
 from typing import Any, Dict, List, Optional, Union
 
-from sqlalchemy import BOOLEAN, INT, TEXT
-from sqlalchemy import Column
-from sqlalchemy import ForeignKey
-from sqlalchemy.orm import relationship
-from sqlalchemy_utc import UtcDateTime
+import logging
 
 from pajbot import utils
 from pajbot.apiwrappers.base import BaseAPI
 from pajbot.apiwrappers.twitch.base import BaseTwitchAPI
-from pajbot.apiwrappers.twitch.helix import TwitchVideo, TwitchGame
-from pajbot.managers.db import Base
-from pajbot.managers.db import DBManager
+from pajbot.apiwrappers.twitch.helix import TwitchGame, TwitchVideo
+from pajbot.managers.db import Base, DBManager
 from pajbot.managers.handler import HandlerManager
 from pajbot.managers.redis import RedisManager
+from pajbot.models.user import UserChannelInformation, UserStream
 
-from pajbot.models.user import UserStream, UserChannelInformation
+from sqlalchemy import BOOLEAN, INT, TEXT, Column, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy_utc import UtcDateTime
 
 log = logging.getLogger("pajbot")
 
@@ -58,8 +56,7 @@ class Stream(Base):
         StreamChunk, uselist=True, backref="stream", cascade="save-update, merge, expunge", lazy="joined"
     )
 
-    def __init__(self, created_at, **options):
-        self.id = None
+    def __init__(self, created_at: str, **options: Any) -> None:
         self.title = options.get("title", "NO TITLE")
         self.stream_start = BaseAPI.parse_datetime(created_at)
         self.stream_end = None
