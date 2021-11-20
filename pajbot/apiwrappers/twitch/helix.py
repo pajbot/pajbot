15f9040195b466b75da538889d5934f7c6300dc0
ALazyMeme <12804673+ALazyMeme@users.noreply.github.com>
2021-11-20 22:34:54 +1000

    Add `require_login` function

--- pajbot/apiwrappers/twitch/helix.py
+++ pajbot/apiwrappers/twitch/helix.py
@@ -226,6 +226,12 @@ class TwitchHelixAPI(BaseTwitchAPI):
             raise ValueError(f'No user found under login name "{login}" on Twitch')
         return user_id
 
+    def require_login(self, user_id):
+        login = self.get_login(user_id)
+        if login is None:
+            raise ValueError(f'No user found under user ID "{user_ud}" on Twitch')
+        return login
+
     def get_login(self, user_id: str) -> Optional[str]:
         """Gets the twitch login name as a string for the given twitch login name,
         utilizing a cache or the twitch API on cache miss.
