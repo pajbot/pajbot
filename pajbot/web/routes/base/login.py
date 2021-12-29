ab761ce1ecb46c246e47e260bbda5fcb0a8bde30
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2021-12-29 16:50:28 +0100

    fix code

--- pajbot/web/routes/base/login.py
+++ pajbot/web/routes/base/login.py
@@ -27,7 +27,7 @@ def init(app):
         params = {
             "client_id": app.bot_config["twitchapi"]["client_id"],
             "redirect_uri": app.bot_config["twitchapi"].get(
-                "redirect_uri", f"https://{config['web']['domain']}/login/authorized"
+                "redirect_uri", f"https://{app.bot_config['web']['domain']}/login/authorized"
             ),
             "response_type": "code",
             "scope": " ".join(scopes),
