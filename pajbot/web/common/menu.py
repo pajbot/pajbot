cbfd7f83500250646fb92dc80c3f62d82d11cd8a
ALazyMeme <github@alazymeme.com>
2022-04-02 19:34:39 +1000

    Add same functionality for the admin navbar

--- pajbot/web/common/menu.py
+++ pajbot/web/common/menu.py
@@ -52,10 +52,18 @@ def init(app):
     nav_bar_admin_header.append(MenuItem("/admin/timers", "admin_timers", "Timers"))
     nav_bar_admin_header.append(MenuItem("/admin/moderators", "admin_moderators", "Moderators"))
     nav_bar_admin_header.append(MenuItem("/admin/modules", "admin_modules", "Modules"))
-    if "playsounds" in app.bot_modules:
-        nav_bar_admin_header.append(MenuItem("/admin/playsounds", "admin_playsounds", "Playsounds"))
-    if "predict" in app.module_manager:
-        nav_bar_admin_header.append(MenuItem("/admin/predictions", "admin_predictions", "Predictions"))
+
+    if app.bot_modules:
+        if "playsounds" in app.bot_modules:
+            nav_bar_admin_header.append(MenuItem("/admin/playsounds", "admin_playsounds", "Playsounds"))
+        if "predict" in app.bot_modules:
+            nav_bar_admin_header.append(MenuItem("/admin/predictions", "admin_predictions", "Predictions"))
+    else:
+        if "playsound" in app.module_manager:
+            nav_bar_admin_header.append(MenuItem("/admin/playsounds", "admin_playsounds", "Playsounds"))
+        if "predict" in app.module_manager:
+            nav_bar_admin_header.append(MenuItem("/admin/predictions", "admin_predictions", "Predictions"))
+
     nav_bar_admin_header.append(MenuItem("/admin/streamer", "admin_streamer", "Streamer Info"))
 
     @app.context_processor
