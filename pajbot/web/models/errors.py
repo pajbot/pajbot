ffdacd455ed44ceb6696350a18d5578db83b509b
pajlada <rasmus.karlsson@pajlada.com>
2022-04-09 12:12:17 +0200

    Fix errors in the API not properly returning a JSON response (#1833)

--- pajbot/web/models/errors.py
+++ pajbot/web/models/errors.py
@@ -9,18 +9,30 @@ def init(app, config):
     @app.errorhandler(404)
     def page_not_found(e):
         if request.path.startswith("/api/"):
-            return jsonify({"error": "No API endpoint here!"})
+            return jsonify({"error": "No API endpoint here!"}), 404
         return render_template("errors/404.html"), 404
 
     @app.errorhandler(500)
     def internal_server_errors(e):
+        if request.path.startswith("/api/"):
+            return jsonify({"error": "Internal server error"}), 500
         return render_template("errors/500.html"), 500
 
     @app.errorhandler(403)
     def forbidden(e):
+        if request.path.startswith("/api/"):
+            return jsonify({"error": "Method not allowed"}), 403
         return render_template("errors/403.html"), 403
+
+    @app.errorhandler(405)
+    def method_not_allowed(e):
+        if request.path.startswith("/api/"):
+            return jsonify({"error": "Method not allowed"}), 405
+        return render_template("errors/405_method_not_allowed.html"), 405
 
     @app.errorhandler(Exception)
     def all_exception_handler(error):
         log.exception("Unhandled exception")
+        if request.path.startswith("/api/"):
+            return jsonify({"error": "Internal server error - Unhandled exception"}), 500
         return render_template("errors/500_unhandled.html"), 500
