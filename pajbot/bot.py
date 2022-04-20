c51cbd9ec51576eadb38df907076e7afbff4fc11
matt <30363562+mmattbtw@users.noreply.github.com>
2022-04-20 15:33:18 -0500

    added `safe_announcement`

--- pajbot/bot.py
+++ pajbot/bot.py
@@ -788,6 +788,10 @@ class Bot:
     def safe_say(self, message: str, channel: Optional[str] = None) -> None:
         if not self.is_bad_message(message):
             self.say(message, channel)
+            
+    def safe_announcement(self, message:str, channel: Optional[str] = None) -> None:
+        if not self.is_bad_message(message):
+            self.announcement(message, channel)
 
     def me(self, message: str, channel: Optional[str] = None) -> None:
         self.say("/me " + message[: CHARACTER_LIMIT - 4], channel=channel)
