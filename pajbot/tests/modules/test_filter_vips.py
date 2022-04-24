33319e9ebff70aeff22c8c5bcff1a4834e47227a
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2022-04-24 11:36:49 +0200

    Reformat test

--- pajbot/tests/modules/test_filter_vips.py
+++ pajbot/tests/modules/test_filter_vips.py
@@ -1,34 +1,28 @@
 from typing import List, Tuple
 
-import pytest
-
 from pajbot.modules.vip_refresh import VIPRefreshModule
+
+import pytest
 
 
 def get_filter_vips_cases() -> List[Tuple[List[str], List[str]]]:
     return [
-            # Standard login names
-            (["foo"], ["foo"]),
-            (["pajlada", "forsen"], ["pajlada", "forsen"]),
-
-            # Display names where they're ascii-only but contain uppercase characters
-            (["pajlada", "FORSEN"], ["pajlada", "FORSEN"]),
-
-            # Display names with spaces in the middle should be filtered out
-            (["pajlada", "Riot Games", "forsen"], ["pajlada", "forsen"]),
-
-            # Display names with spaces at the end should be filtered out
-            (["pajlada", "testman ", "forsen"], ["pajlada", "forsen"]),
-
-            # Display names with spaces at the start should be filtered out
-            (["pajlada", " testman", "forsen"], ["pajlada", "forsen"]),
-
-            # Display names non-ascii characters should be filtered out
-            (["pajlada", "테스트계정420", "forsen"], ["pajlada", "forsen"]),
-
-            # Empty list
-            ([], []),
-            ]
+        # Standard login names
+        (["foo"], ["foo"]),
+        (["pajlada", "forsen"], ["pajlada", "forsen"]),
+        # Display names where they're ascii-only but contain uppercase characters
+        (["pajlada", "FORSEN"], ["pajlada", "FORSEN"]),
+        # Display names with spaces in the middle should be filtered out
+        (["pajlada", "Riot Games", "forsen"], ["pajlada", "forsen"]),
+        # Display names with spaces at the end should be filtered out
+        (["pajlada", "testman ", "forsen"], ["pajlada", "forsen"]),
+        # Display names with spaces at the start should be filtered out
+        (["pajlada", " testman", "forsen"], ["pajlada", "forsen"]),
+        # Display names non-ascii characters should be filtered out
+        (["pajlada", "테스트계정420", "forsen"], ["pajlada", "forsen"]),
+        # Empty list
+        ([], []),
+    ]
 
 
 @pytest.mark.parametrize("input_vips,expected_vips", get_filter_vips_cases())
