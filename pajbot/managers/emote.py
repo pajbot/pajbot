1f4a306c893ccd2a84515fe1328de95eed22dfea
Foretack <observer.in.a.box@gmail.com>
2022-08-06 21:05:06 +0200

    Fix wrong information on Twitch emotes comment

--- pajbot/managers/emote.py
+++ pajbot/managers/emote.py
@@ -220,7 +220,7 @@ class EmoteManager:
                 "2": EmoteManager.twitch_emote_url(emote_id, "2.0"),
                 "4": EmoteManager.twitch_emote_url(emote_id, "3.0"),
             },
-            # Twitch doesn't provide the dimensions of emotes. However, subscriber emotes are always size-fixed
+            # Twitch gives no data regarding this, but it can be assumed that this will be the emote size most of the time 
             max_width=112,
             max_height=112
         )
