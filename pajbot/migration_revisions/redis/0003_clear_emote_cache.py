49ac6377e59f63178f0f2246a033f4ba840cd5cb
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2022-08-07 15:44:52 +0200

    Rename emote cache clearing migration

--- pajbot/migration_revisions/redis/0003_clear_emote_cache.py
+++ pajbot/migration_revisions/redis/0003_clear_emote_cache.py
@@ -1,7 +1,9 @@
 import json
 
+
 def up(redis, bot):
-    # invalidate all emote caches; they don't contain the max_width and max_height properties
+    # We have updated the Emote model, invalidate all emote caches
+    # Specifically, the max_width and max_height properties are new and will be missing from the caches
     redis.delete(f"api:ffz:channel-emotes:{bot.streamer.login}")
     redis.delete("api:ffz:global-emotes")
     redis.delete(f"api:7tv:channel-emotes:{bot.streamer.login}")
@@ -15,10 +17,7 @@ def up(redis, bot):
     current_emote_key = f"{bot.streamer.login}:current_quest_emote"
     current_emote_json = redis.get(current_emote_key)
     if current_emote_json is not None:
-        # ensure emote id is a string
+        # Force set max_width and max_height for the quest emote
         current_emote = json.loads(current_emote_json)
         current_emote["max_width"] = 112
         current_emote["max_height"] = 112
-        if not isinstance(current_emote["id"], str):
-            current_emote["id"] = str(current_emote["id"])
-            redis.set(current_emote_key, json.dumps(current_emote))
