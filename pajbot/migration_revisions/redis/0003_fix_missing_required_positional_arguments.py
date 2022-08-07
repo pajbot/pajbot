fa6f45b184e4a59d1c1f4bf148490c400ee4f93d
Foretack <observer.in.a.box@gmail.com>
2022-08-07 15:41:04 +0200

    Add quest emote property updating

--- pajbot/migration_revisions/redis/0003_fix_missing_required_positional_arguments.py
+++ pajbot/migration_revisions/redis/0003_fix_missing_required_positional_arguments.py
@@ -1,3 +1,5 @@
+import json
+
 def up(redis, bot):
     # invalidate all emote caches; they don't contain the max_width and max_height properties
     redis.delete(f"api:ffz:channel-emotes:{bot.streamer.login}")
@@ -9,3 +11,14 @@ def up(redis, bot):
     redis.delete(f"api:twitch:helix:channel-emotes:{bot.streamer.id}")
     redis.delete(f"api:twitch:helix:stream:by-id:{bot.streamer.id}")
     redis.delete("api:twitch:helix:global-emotes")
+
+    current_emote_key = f"{bot.streamer.login}:current_quest_emote"
+    current_emote_json = redis.get(current_emote_key)
+    if current_emote_json is not None:
+        # ensure emote id is a string
+        current_emote = json.loads(current_emote_json)
+        current_emote["max_width"] = 112
+        current_emote["max_height"] = 112
+        if not isinstance(current_emote["id"], str):
+            current_emote["id"] = str(current_emote["id"])
+            redis.set(current_emote_key, json.dumps(current_emote))
