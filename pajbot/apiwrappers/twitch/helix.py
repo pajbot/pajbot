fc1936e864e714106f44fea5fb63c16de2e1daaa
ALazyMeme <12804673+ALazyMeme@users.noreply.github.com>
2022-10-31 22:00:23 +1000

    Add ban_user function

--- pajbot/apiwrappers/twitch/helix.py
+++ pajbot/apiwrappers/twitch/helix.py
@@ -781,3 +781,10 @@ class TwitchHelixAPI(BaseTwitchAPI):
         end_time = response["data"][0]["end_time"]
 
         return created_at, end_time
+
+    def ban_user(self, broadcaster_id: str, bot_id: str, authorization, reason: str, user_id: str) -> str:
+        """Calls the _ban_user function using the reason & user_id parameters.
+        All fields are required. bot_id must match the user_id in authorization."""
+        response = self._ban_user(broadcaster_id, bot_id, authorization, reason, user_id)
+
+        return response[0]
