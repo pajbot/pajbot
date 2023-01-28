08a0d9bf5676dbb20303a28e25b152bca081392f
iProdigy <gitProdigy@proton.me>
2023-01-27 17:28:43 -0800

    fix(helix): only consider subs with names

--- pajbot/apiwrappers/twitch/helix.py
+++ pajbot/apiwrappers/twitch/helix.py
@@ -349,7 +349,7 @@ class TwitchHelixAPI(BaseTwitchAPI):
         # }
 
         subscribers = [
-            UserBasics(entry["user_id"], entry["user_login"], entry["user_name"]) for entry in response["data"]
+            UserBasics(entry["user_id"], entry["user_login"], entry["user_name"]) for entry in response["data"] if entry["user_login"]
         ]
         pagination_cursor = response["pagination"].get("cursor", None)
 
