2fafe7b279ae1b86b09d99efee36e3b6d99078b1
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2023-03-25 13:22:27 +0100

    roulette: fix user_id type

--- pajbot/models/roulette.py
+++ pajbot/models/roulette.py
@@ -4,7 +4,7 @@ import logging
 from pajbot import utils
 from pajbot.managers.db import Base
 
-from sqlalchemy import ForeignKey, Integer
+from sqlalchemy import ForeignKey, Integer, Text
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy_utc import UtcDateTime
 
@@ -15,11 +15,11 @@ class Roulette(Base):
     __tablename__ = "roulette"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("user.id", ondelete="CASCADE"), index=True)
+    user_id: Mapped[str] = mapped_column(Text, ForeignKey("user.id", ondelete="CASCADE"), index=True)
     created_at: Mapped[datetime.datetime] = mapped_column(UtcDateTime())
     points: Mapped[int]
 
-    def __init__(self, user_id: int, points: int) -> None:
+    def __init__(self, user_id: str, points: int) -> None:
         self.user_id = user_id
         self.created_at = utils.now()
         self.points = points
