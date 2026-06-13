808927704976bd685520a239fd75bbdd9f29677d
Rasmus Karlsson <rasmus.karlsson@pajlada.com>
2026-06-13 09:55:44 +0200

    ci: use test.sh script
    this allows me to configure some things like environment variables
    easier that i want to use for testing

---
+++ scripts/test.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+export PYTEST_ADDOPTS="-W error::DeprecationWarning"
+
+if ! command -v uv >/dev/null; then
+    echo "This script requires uv installed. You can manually run tests with pytest in your venv."
+    exit 1
+fi
+
+if [ ! -d scripts ]; then
+    echo "This script needs to be called from the root folder, i.e. ./scripts/venvinstall.sh"
+    exit 1
+fi
+
+uv run pytest pajbot
